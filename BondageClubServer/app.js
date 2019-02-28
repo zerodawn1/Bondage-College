185eb8abe74499619d4dcaed667c15fb3429a3bc
Ben987 <30805159+Ben987@users.noreply.github.com>
2019-02-27 19:19:23 -0500

    First Server Tests with MongoDB
    First Server Tests with MongoDB

--- BondageClubServer/app.js
+++ BondageClubServer/app.js
@@ -1,18 +1,30 @@
 // Main game objects
-var fs = require('fs');
-var app = require('http').createServer()
-var io = require('socket.io')(app);
-var bcrypt = require('bcrypt');
+var App = require('http').createServer()
+var IO = require('socket.io')(App);
+var BCrypt = require('bcrypt');
 var Account = [];
+var Database;
+var DatabaseClient = require('mongodb').MongoClient;
+var DatabaseURL = "mongodb://localhost:27017/BondageClubDatabase";
 
 // Listens for clients on port 4288
-app.listen(4288);
-console.log("*************************************");
-console.log("Bondage Club Server Listening on 4288");
-console.log("*************************************");
+App.listen(4288, function () {
+	console.log("*************************************");
+	console.log("Bondage Club Server Listening on 4288");
+	console.log("*************************************");	
+});
+
+// Connects to the Mongo Database
+DatabaseClient.connect(DatabaseURL, { useNewUrlParser: true }, function(err, db) {
+	if (err) throw err;
+	console.log("BondageClubDatabase connected");
+	Database = db.db("BondageClubDatabase");
+	//Database.collection("Accounts").findOne({}, function(err, result) { console.log(result); });
+	//Database.collection("Accounts").deleteMany({}, function(err, result) { console.log("All accounts delete"); });
+});
 
 // Client/Server events
-io.on("connection", function (socket) {
+IO.on("connection", function (socket) {
 	socket.id = Math.round(Math.random() * 1000000000000);
 	socket.emit("ServerMessage", "Connected to the Bondage Club Server");
 	socket.on("AccountCreate", function (data) { AccountCreate(data, socket) });
@@ -29,66 +41,82 @@ function CommonTime() {
 
 // Creates a new account by creating its file
 function AccountCreate(data, socket) {
-	
-	// Checks if the file already exists
-	var FileName = "accounts/" + data.AccountName + ".json";
-	fs.stat(FileName, function(err, stat) {
-		if(err == null) {
-			socket.emit("CreationResponse", "Account already exists");
-		} else if(err.code === 'ENOENT') {
-		
-			// Since the file doesn't exist, we create it with the hashed password
-			bcrypt.hash(data.Password, 10, function( err, hash ) {
-				if (err) throw err;
-				console.log("Creating new account: " + data.AccountName + "  ID: " + socket.id.toString());
-				data.Password = hash;
-				data.ID = socket.id;
-				Account.push(data);
-				AccountSave(data);
-				socket.emit("CreationResponse", "AccountCreated" + CommonTime().toString());
-			});
 
+	// Makes sure the account comes with a name and a password
+	if ((data.AccountName != null) && (data.Password != null)) {
+	
+		// Checks if the account already exists
+		Database.collection("Accounts").findOne({ AccountName : data.AccountName }, function(err, result) {
+
+			// Makes sure the result is null so the account doesn't already exists
+			if (err) throw err;
+			if (result != null) {
+				socket.emit("CreationResponse", "Account already exists");			
+			} else {
 			
-		} else {
-			socket.emit("CreationResponse", "Error: " + err.code);
-		}
-		
-	});
+				// Since the file doesn't exist, we create it with the hashed password
+				BCrypt.hash(data.Password, 10, function( err, hash ) {
+					if (err) throw err;
+					console.log("Creating new account: " + data.AccountName + "  ID: " + socket.id.toString());
+					data.Password = hash;
+					data.ID = socket.id;
+					Account.push(data);
+					Database.collection("Accounts").insertOne(data, function(err, res) {
+						if (err) throw err;
+						console.log("Account created");
+					});
+					socket.emit("CreationResponse", "AccountCreated" + CommonTime().toString());
+				});
+				
+			}
+			
+		});
+
+	} else socket.emit("CreationResponse", "Invalid account information");
+	
 }
 
 // Load a single account file
 function AccountLogin(data, socket) {
+
+	// Makes sure the login comes with a name and a password
 	if ((data.AccountName != null) && (data.Password != null)) {
-		fs.readFile("accounts/" + data.AccountName + ".json", function(err, data) {
-			if (err) {
+
+		// Checks if there's an account that matches the name 
+		Database.collection("Accounts").findOne({ AccountName : data.AccountName}, function(err, result) {	
+			if (err) throw err;
+			if (result === null) {
 				socket.emit("LoginResponse", "InvalidNamePassword");
 			}
 			else {
-				var C = JSON.parse(data);
-				console.log("Login account: " + C.AccountName + "  ID: " + socket.id.toString());
-				socket.emit("LoginResponse", C);
-				C.ID = socket.id;
-				Account.push(C);
-			}
-		});
-	} else socket.emit("LoginResponse", "InvalidNamePassword");
-}
 
-// Saves the account data to a file
-function AccountSave(data) {
-	fs.writeFile("accounts/" + data.AccountName + ".json", JSON.stringify(data), function (err) {
-		if (err) throw err;
-	});
+				// Compare the password to its hashed version
+				BCrypt.compare(data.Password, result.Password, function( err, res ) {
+					if (res) {
+						console.log("Login account: " + result.AccountName + "  ID: " + socket.id.toString());
+						socket.emit("LoginResponse", result);
+						result.ID = socket.id;
+						Account.push(result);
+					} else socket.emit("LoginResponse", "InvalidNamePassword");
+				});
+				
+			}			
+		});
+		
+	} else socket.emit("LoginResponse", "InvalidNamePassword");
 }
 
 // Updates the account appearance
 function AccountUpdate(data, socket) {
 	for(var P = 0; P < Account.length; P++)
-		if (Account[P].ID == socket.id) {
+		if (Account[P].ID == socket.id) { 
 			console.log("Updating account: " + Account[P].AccountName + "  ID: " + socket.id.toString());
 			if (data.Appearance != null) Account[P].Appearance = data.Appearance;
 			if (data.AssetFamily != null) Account[P].AssetFamily = data.AssetFamily;
-			AccountSave(Account[P]);
+			Database.collection("Accounts").updateOne({ AccountName : Account[P].AccountName }, { $set: Account[P] }, function(err, res) {
+				if (err) throw err;
+				console.log("Account updated");
+			});
 		}
 }
 
