1a81e96b7fdb67f09a6551e2be11b951159d8d64
adokilume <43858488+adokilume@users.noreply.github.com>
2019-03-17 17:44:52 +0100

    German Bondage Club Beginnings + Translation Tools (#117)
    * German Bondage Club Beginnings + Translation Tools
    
    * German Bondage Club: Shibari Dojo
    
    * German Bondage Club: Management

--- BondageClub/Scripts/Translation.js
+++ BondageClub/Scripts/Translation.js
@@ -17,8 +17,19 @@ var TranslationDictionary = [
 		LanguageName: "Deutsch",
 		EnglishName: "German",
 		Files: [
+			"Screens/Character/Login/Text_Login_DE.txt",
+			"Screens/Cutscene/PlayerMistress/Text_PlayerMistress_DE.txt",
 			"Screens/Room/Introduction/Dialog_NPC_Introduction_Maid_DE.txt",
-			"Screens/Character/Login/Text_Login_DE.txt"
+			"Screens/Room/Introduction/Dialog_NPC_Introduction_Sub_DE.txt",
+			"Screens/Room/MaidQuarters/Dialog_NPC_MaidQuarters_InitiationMaids_DE.txt",
+			"Screens/Room/MaidQuarters/Dialog_NPC_MaidQuarters_Maid_DE.txt",
+			"Screens/Room/MainHall/Dialog_NPC_MainHall_Maid_DE.txt",
+			"Screens/Room/MainHall/Text_MainHall_DE.txt",
+			"Screens/Room/Management/Dialog_NPC_Management_Mistress_DE.txt",
+			"Screens/Room/Management/Dialog_NPC_Management_RandomGirl_DE.txt",
+			"Screens/Room/Management/Dialog_NPC_Management_Sub_DE.txt",
+			"Screens/Room/Shibari/Dialog_NPC_Shibari_Student_DE.txt",
+			"Screens/Room/Shibari/Dialog_NPC_Shibari_Teacher_DE.txt"
 		]
 	},
 	{
