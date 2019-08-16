b034717df4c1debdce21c2dc1a65264e5fdb1983
gatetrek <52715435+gatetrek@users.noreply.github.com>
2019-08-16 03:59:12 +0100

    fixes and layers for the plug gag (#266)

--- BondageClub/Screens/Inventory/ItemMouth/PlugGag/PlugGag.js
+++ BondageClub/Screens/Inventory/ItemMouth/PlugGag/PlugGag.js
@@ -2,7 +2,7 @@
 
 // Loads the item extension properties
 function InventoryItemMouthPlugGagLoad() {
-	if (DialogFocusItem.Property == null) DialogFocusItem.Property = { Type: null, Effect: ["BlindTotal"] };
+	if (DialogFocusItem.Property == null) DialogFocusItem.Property = { Type: null, Effect: ["GagHeavy"] };
 }
 
 // Draw the item extension screen
@@ -16,19 +16,19 @@ function InventoryItemMouthPlugGagDraw() {
 
 	// Draw the possible gag types
 	DrawText(DialogFind(Player, "SelectGagType"), 1500, 500, "white", "gray");
-	DrawButton(1175, 550, 225, 225, "", (DialogFocusItem.Property.Type == null || DialogFocusItem.Property.Type == "Plug") ? "#888888" : "White");
-	DrawImage("Screens/Inventory/" + DialogFocusItem.Asset.Group.Name + "/" + DialogFocusItem.Asset.Name + "/Plug.png", 1175, 549);
-	DrawText(DialogFind(Player, "PlugGagMouthTypePlug"), 1288, 800, "white", "gray");
-	DrawButton(1600, 550, 225, 225, "", ((DialogFocusItem.Property.Type != null) && (DialogFocusItem.Property.Type == "Open")) ? "#888888" : "White");
-	DrawImage("Screens/Inventory/" + DialogFocusItem.Asset.Group.Name + "/" + DialogFocusItem.Asset.Name + "/Open.png", 1600, 549);
-	DrawText(DialogFind(Player, "PlugGagMouthTypeOpen"), 1713, 800, "white", "gray");
+	DrawButton(1175, 550, 225, 225, "", (DialogFocusItem.Property.Type == null || DialogFocusItem.Property.Type == "Open") ? "#888888" : "White");
+	DrawImage("Screens/Inventory/" + DialogFocusItem.Asset.Group.Name + "/" + DialogFocusItem.Asset.Name + "/Open.png", 1175, 549);
+	DrawText(DialogFind(Player, "PlugGagMouthTypeOpen"), 1288, 800, "white", "gray");
+	DrawButton(1600, 550, 225, 225, "", ((DialogFocusItem.Property.Type != null) && (DialogFocusItem.Property.Type == "Plug")) ? "#888888" : "White");
+	DrawImage("Screens/Inventory/" + DialogFocusItem.Asset.Group.Name + "/" + DialogFocusItem.Asset.Name + "/Plug.png", 1600, 549);
+	DrawText(DialogFind(Player, "PlugGagMouthTypePlug"), 1713, 800, "white", "gray");
 }
 
 // Catches the item extension clicks
 function InventoryItemMouthPlugGagClick() {
 	if ((MouseX >= 1885) && (MouseX <= 1975) && (MouseY >= 25) && (MouseY <= 110)) DialogFocusItem = null;
 	if ((MouseX >= 1175) && (MouseX <= 1400) && (MouseY >= 550) && (MouseY <= 775) && (DialogFocusItem.Property.Type != null)) InventoryItemMouthPlugGagSetType(null);
-	if ((MouseX >= 1600) && (MouseX <= 1825) && (MouseY >= 550) && (MouseY <= 775) && ((DialogFocusItem.Property.Type == null) || (DialogFocusItem.Property.Type != "Open"))) InventoryItemMouthPlugGagSetType("Open");
+	if ((MouseX >= 1600) && (MouseX <= 1825) && (MouseY >= 550) && (MouseY <= 775) && ((DialogFocusItem.Property.Type == null) || (DialogFocusItem.Property.Type != "Plug"))) InventoryItemMouthPlugGagSetType("Plug");
 }
 
 // Sets the gag type (Plug, Open)
@@ -39,8 +39,8 @@ function InventoryItemMouthPlugGagSetType(NewType) {
 		InventoryItemMouthPlugGagLoad();
 	}
 	DialogFocusItem.Property.Type = NewType;
-	if (NewType == null) DialogFocusItem.Property.Effect = ["GagTotal"];
-	else if (NewType == "Open") DialogFocusItem.Property.Effect = ["GagHeavy"];
+	if (NewType == null) DialogFocusItem.Property.Effect = ["GagHeavy"];
+	else if (NewType == "Plug") DialogFocusItem.Property.Effect = ["GagTotal"];
 
 	// Adds the lock effect back if it was padlocked
 	if ((DialogFocusItem.Property.LockedBy != null) && (DialogFocusItem.Property.LockedBy != "")) {
