c03e462647d19ffeb2175c47f8aa2598677438ed
Ben987 <30805159+Ben987@users.noreply.github.com>
2020-01-15 22:04:48 -0500

    Blocked Whisper Fix
    Blocked Whisper Fix

--- BondageClub/Screens/Inventory/ItemArms/FullLatexSuit/FullLatexSuit.js
+++ BondageClub/Screens/Inventory/ItemArms/FullLatexSuit/FullLatexSuit.js
@@ -17,40 +17,38 @@ function InventoryItemArmsFullLatexSuitLoad() {
 
 // Draw the item extension screen
 function InventoryItemArmsFullLatexSuitDraw() {
+	
 	var C = (Player.FocusGroup != null) ? Player : CurrentCharacter;
-
 	DrawRect(1387, 125, 225, 275, "white");
 	DrawText(DialogFind(Player, "SelectSuitType"), 1500, 50, "white", "gray");
 	DrawImageResize("Assets/" + DialogFocusItem.Asset.Group.Family + "/" + DialogFocusItem.Asset.Group.Name + "/Preview/" + DialogFocusItem.Asset.Name + ".png", 1389, 127, 221, 221);
 	DrawTextFit(DialogFocusItem.Asset.Description, 1500, 375, 221, "black");
 	
-// draw suits to wear	
+	// draw suits to wear	
 	DrawButton(1150, 450, 225, 225, "", (DialogFocusItem.Property.Type == null || DialogFocusItem.Property.Type == "Latex") ? "#888888" : "White");
 	DrawImage("Screens/Inventory/" + DialogFocusItem.Asset.Group.Name + "/" + DialogFocusItem.Asset.Name + "/Latex.png", 1150, 449);
 	DrawText(DialogFind(Player, "FullLatexSuitTypeZipped"), 1263, 425, "white", "gray");
 	DrawButton(1600, 450, 225, 225, "", ((DialogFocusItem.Property.Type != null) && (DialogFocusItem.Property.Type == "UnZip")) ? "#888888" : "White");
 	DrawImage("Screens/Inventory/" + DialogFocusItem.Asset.Group.Name + "/" + DialogFocusItem.Asset.Name + "/UnZip.png", 1600, 449);
 	DrawText(DialogFind(Player, "FullLatexSuitTypeUnZip"), 1713, 425, "white", "gray");
-	if(InventoryGet(C, "ItemVulvaPiercings") == null || 
-		
+	if (InventoryGet(C, "ItemVulvaPiercings") == null ||
 		!InventoryGet(C, "ItemVulvaPiercings").Asset ||
 		!InventoryGet(C, "ItemVulvaPiercings").Asset.Effect ||
-		InventoryGet(C, "ItemVulvaPiercings").Asset.Effect.indexOf("Chaste") == -1)
-		
-	{	
+		InventoryGet(C, "ItemVulvaPiercings").Asset.Effect.indexOf("Chaste") == -1)	{	
 		DrawButton(1375, 750, 225, 225, "", (InventoryGet(C, "ItemVulva") == null) ? "White" : "White");
 		DrawImage("Screens/Inventory/" + DialogFocusItem.Asset.Group.Name + "/" + DialogFocusItem.Asset.Name + "/Wand.png", 1375, 749);
-		DrawText(DialogFind(Player, "FullLatexSuitTypeWand"), 1488, 750, "white", "gray");
+		DrawText(DialogFind(Player, "FullLatexSuitTypeWand"), 1488, 725, "white", "gray");
 	}
 	if (InventoryItemArmsFullLatexSuitMsg != null) DrawTextWrap(DialogFind(Player, InventoryItemArmsFullLatexSuitMsg), 1100, 850, 800, 160, "White");
+
 }
 
 // Catches the item extension clicks
 function InventoryItemArmsFullLatexSuitClick() {
 	if ((MouseX >= 1885) && (MouseX <= 1975) && (MouseY >= 25) && (MouseY <= 110)) DialogFocusItem = null;
-	if ((MouseX >= 1150) && (MouseX <= 1400) && (MouseY >= 450) && (MouseY <= 675) && (DialogFocusItem.Property.Type != null)) InventoryItemArmsFullLatexSuitSetType(null);
+	if ((MouseX >= 1150) && (MouseX <= 1375) && (MouseY >= 450) && (MouseY <= 675) && (DialogFocusItem.Property.Type != null)) InventoryItemArmsFullLatexSuitSetType(null);
 	if ((MouseX >= 1600) && (MouseX <= 1825) && (MouseY >= 450) && (MouseY <= 675) && ((DialogFocusItem.Property.Type == null) || (DialogFocusItem.Property.Type != "UnZip"))) InventoryItemArmsFullLatexSuitSetType("UnZip");
-		var C = (Player.FocusGroup != null) ? Player : CurrentCharacter;
+	var C = (Player.FocusGroup != null) ? Player : CurrentCharacter;
 	if (CommonIsClickAt(1375, 750, 225, 225) && InventoryGet(C, "ItemVulva") == null) InventoryItemArmsFullLatexSuitSetType("Wand");
 }
 
--- BondageClub/Screens/Online/ChatRoom/ChatRoom.js
+++ BondageClub/Screens/Online/ChatRoom/ChatRoom.js
@@ -52,6 +52,14 @@ function ChatRoomLoad() {
 	ChatRoomCreateElement();
 }
 
+// Returns TRUE if the player owner is inside the room
+function ChatRoomOwnerInside() {
+	for (var C = 0; C < ChatRoomCharacter.length; C++)
+		if (Player.Ownership.MemberNumber == ChatRoomCharacter[C].MemberNumber)
+			return true;
+	return false;
+}
+
 // Draw the characters in the room
 function ChatRoomDrawCharacter(DoClick) {
 
@@ -99,7 +107,7 @@ function ChatRoomDrawCharacter(DoClick) {
 					if (ChatRoomCharacter[C].ID != 0) ServerSend("AccountOwnership", { MemberNumber: ChatRoomCharacter[C].MemberNumber });
 					CharacterSetCurrent(ChatRoomCharacter[C]);
 				} else {
-					if (!LogQuery("BlockWhisper", "OwnerRule") || (Player.Ownership == null) || (Player.Ownership.Stage != 1) || (Player.Ownership.MemberNumber == ChatRoomCharacter[C].MemberNumber))
+					if (!LogQuery("BlockWhisper", "OwnerRule") || (Player.Ownership == null) || (Player.Ownership.Stage != 1) || (Player.Ownership.MemberNumber == ChatRoomCharacter[C].MemberNumber) || !ChatRoomOwnerInside())
 						ChatRoomTargetMemberNumber = ((ChatRoomTargetMemberNumber == ChatRoomCharacter[C].MemberNumber) || (ChatRoomCharacter[C].ID == 0)) ? null : ChatRoomCharacter[C].MemberNumber;
 				}
 				break;
