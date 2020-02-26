52fb11053dd00c02df2b6b9e416f64e090309fba
Ben987 <30805159+Ben987@users.noreply.github.com>
2020-02-26 10:11:54 -0500

    Prevent Using Alpha Hoods with Chain Hogties
    Prevent Using Alpha Hoods with Chain Hogties

--- BondageClub/Screens/Inventory/ItemArms/Chains/Chains.js
+++ BondageClub/Screens/Inventory/ItemArms/Chains/Chains.js
@@ -53,7 +53,7 @@ function InventoryItemArmsChainsSetPose(NewType) {
 	}
 
 	// Validates a few parameters before hogtied
-	if ((NewType != null) && !InventoryAllow(C, ["NotKneeling", "NotMounted", "NotChained", "NotSuspended"], true)) { DialogExtendedMessage = DialogText; return; }
+	if ((NewType != null) && !InventoryAllow(C, ["NotKneeling", "NotMounted", "NotChained", "NotSuspended", "CannotBeHogtiedWithAlphaHood"], true)) { DialogExtendedMessage = DialogText; return; }
 
 	// Sets the new pose with it's effects
 	if (!InventoryItemHasEffect(DialogFocusItem, "Lock", true)) {
