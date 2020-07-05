e092d156f89e2b2e6b19d50c310962ad886114ad
zerodawn1 <66832794+zerodawn1@users.noreply.github.com>
2020-07-05 23:41:57 +0200

    3d model (#1027)

--- BondageClub/Scripts/Drawing3D.js
+++ BondageClub/Scripts/Drawing3D.js
@@ -1,45 +1,19 @@
 "use strict";
-var renderer;
-var scene;
-var camera;
-var model;
+let renderer;
+let scene;
+let camera;
+let model;
 var Draw3DEnabled = false;
 
 function Draw3DLoad() {
-	
-	scene = new THREE.Scene();
-	camera = new THREE.PerspectiveCamera(45, 2, 0.1, 1000);
-	renderer = new THREE.WebGLRenderer({ alpha: true });
-	renderer.setSize(2000, 1000);
-	renderer.domElement.style.visibility = Draw3DEnabled ? "visible" : "hidden";
-	//renderer.setClearColor(0x000000, 0);
-	document.body.appendChild(renderer.domElement);
-
-	//scene.background = new THREE.Color(0xf1f1f1);
-	scene.background = null;
-	var ambientLight = new THREE.AmbientLight(0xffffff, 1.0 );
-	scene.add(ambientLight);
-						
-    var loader = new THREE.FBXLoader();
-    loader.load('Assets/3D/Rin/Rin1.fbx',
-				function( object ) {
-					model = object;
-					//object.scale.set(0.01, 0.01, 0.01);
-    				scene.add(object);
-    			},
-				undefined,
-				function( error ) {
-					console.log(error);
-				}
-    );
-
+	init();
 	renderer.domElement.addEventListener("click", Click);
 	renderer.domElement.addEventListener("touchstart", Touch);
 	renderer.domElement.addEventListener("mousemove", MouseMove);
 	renderer.domElement.addEventListener("mouseleave", LoseFocus);
 	renderer.domElement.addEventListener("keydown", Draw3DKeyDown);
 	document.body.addEventListener("keydown", Draw3DKeyDown);
-
+	document.body.appendChild(renderer.domElement);
 }
 
 function Draw3DKeyDown(event) {
@@ -47,11 +21,64 @@ function Draw3DKeyDown(event) {
 	if ((KeyCode == 51) && (CurrentScreen == "MainHall") && (CurrentCharacter == null)) Draw3DEnable(!Draw3DEnabled);
 	if ((KeyCode == 37) && Draw3DEnabled) model.rotation.y -= 0.1;
 	if ((KeyCode == 39) && Draw3DEnabled) model.rotation.y += 0.1;
+	if ((KeyCode == 38) && Draw3DEnabled) model.rotation.x -= 0.1;
+	if ((KeyCode == 40) && Draw3DEnabled) model.rotation.x += 0.1;
+}
+
+function init(){
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera(45, window.innerWidth/window.innerHeight,1, 1000);
+
+
+
+	let light = new THREE.DirectionalLight( 0xffffff );
+	light.position.set( 0, 2000, 100 );
+	light.castShadow = true;
+	scene.add( light );
+
+	renderer = new THREE.WebGLRenderer({  alpha : true });
+	renderer.setSize(window.innerWidth, window.innerHeight);
+
+	//when we load an env.
+	// renderer.shadowMap.enabled = false;
+	// renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
+	// renderer.shadowMapDebug = true;
+
+	let ambientLight = new THREE.AmbientLight(0xffffff);
+	ambientLight.castShadow = true;
+	ambientLight.position.set(200,2000,200);
+	scene.add(ambientLight);
+
+    let loader = new THREE.FBXLoader();
+    loader.load('Assets/3D/fbx/maid.fbx',
+				function( object ) {
+					model = object;
+					// animation
+					// model.mixer = new THREE.AnimationMixer(object);
+					// model.mixer = object.mixer;
+					// model.root = object.mixer.getRoot();
+
+					model.castShadow = true;
+					model.reciveShadow = true;
+
+					model.traverse( function (child){
+						if (child.isMesh){
+							child.castShadow = true;
+							child.reciveShadow = true;
+						}
+					});
+					//object.scale.set(0.01, 0.01, 0.01);
+					scene.add(model);
+    			},
+				undefined,
+				function( error ) {
+					console.log(error);
+				}
+    );
 }
 
 function Draw3DEnable(Enable) {
 	Draw3DEnabled = Enable;
-	renderer.domElement.style.visibility = Enable ? "visible" : "hidden";
 	renderer.clear();
 }
 
@@ -62,6 +89,7 @@ function Draw3DProcess() {
 		if (renderer.domElement.style.width != "100%") {
 			renderer.domElement.style.width = "100%";
 			renderer.domElement.style.height = "";
+
 		}
 		if (Draw3DEnabled) renderer.render(scene, camera);
 	}
