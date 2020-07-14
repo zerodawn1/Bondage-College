ceeae95930531b132da530b6c22f03e6c05939be
SandrineBC <61221552+SandrineBC@users.noreply.github.com>
2020-07-14 04:36:43 +0200

    JSDoc comments added (#1079)

--- BondageClub/Screens/Character/FriendList/FriendList.js
+++ BondageClub/Screens/Character/FriendList/FriendList.js
@@ -8,7 +8,10 @@ var FriendListModeIndex = 0;
 var FriendListShowBeep = -1;
 var FriendListBeepLog = [];
 
-// Loads the online friend list from the server
+/**
+ * Loads the online friend list from the server. This function is called dynamically, when the player invokes the friendlist dialog.
+ * @returns {void} - Nothing
+ */
 function FriendListLoad() {
 	FriendListConfirmDelete = [];
 	ElementCreateDiv("FriendList");
@@ -17,7 +20,12 @@ function FriendListLoad() {
 	ServerSend("AccountQuery", { Query: "OnlineFriends" });
 }
 
-// Run the friend list screen - Draw the controls
+// 
+/**
+ * Run the friend list screen - Draw the controls and print the result of the server query to the screen. 
+ * This function is called dynamically on a regular basis. So don't do complex loops within
+ * @returns {void} - Nothing
+ */
 function FriendListRun() {
 	DrawText(TextGet("OnlineFriend"), 230, 35, "White", "Gray");
 	DrawText(TextGet("MemberNumber"), 665, 35, "White", "Gray");
@@ -28,7 +36,10 @@ function FriendListRun() {
 	DrawButton(1935, 5, 60, 60, "", "White", "Icons/Small/Exit.png");
 }
 
-// When the user clicks on the screen
+/**
+ * Handles the click events in the friend list. Clicks are propagated to this function from CommonClick()
+ * @returns {void} - Nothing
+ */
 function FriendListClick() {
 	if ((MouseX >= 1865) && (MouseX < 1925) && (MouseY >= 5) && (MouseY < 65)) {
 		FriendListModeIndex++;
@@ -38,7 +49,11 @@ function FriendListClick() {
 	if ((MouseX >= 1935) && (MouseX < 1995) && (MouseY >= 5) && (MouseY < 65)) FriendListExit();
 }
 
-// when the user exit this screen
+/**
+ * This function is called, when the user exists the friend list. From here we either get back to the InformationSheet 
+ * or the ChatRoom serach, depending on the value of the global variable 'FriendListReturn'
+ * @returns {void} - Nothing
+ */
 function FriendListExit() {
 	ElementRemove("FriendList");
 	if (FriendListReturn != null) {
@@ -47,14 +62,25 @@ function FriendListExit() {
 	} else CommonSetScreen("Character", "InformationSheet");
 }
 
-// Loads the friend list data in the div
+// 
+/**
+ * Loads the friend list data into the HTML div element.
+ * @param {Array.*} data - An array of data, we recieve from the server
+ * @param {string} data.MemberName - The name of the player
+ * @param {number} data.MemberNumber - The ID of the player
+ * @param {string} data.ChatRoomName - The name of the ChatRoom
+ * @param {string} data.ChatRoomSpace - The space, where this room was created. Currently this can be the Asylum or the LARP arena
+ * @param {string} data.Type - The relationship that exists between the player and the friend of the list. 
+ * Currently, only "submissive" is supported
+ * @returns {void} - Nothing
+ */
 function FriendListLoadFriendList(data) {
 
 	// Loads the header caption
 	var BeepCaption = DialogFind(Player, "Beep");
 	var DeleteCaption = DialogFind(Player, "Delete");
 	var ConfirmDeleteCaption = DialogFind(Player, "ConfirmDelete");
-	var PrivateRoomCaption = DialogFind(Player, "PrivateRoom");	
+	var PrivateRoomCaption = DialogFind(Player, "PrivateRoom");
 	var SentCaption = DialogFind(Player, "SentBeep");
 	var ReceivedCaption = DialogFind(Player, "ReceivedBeep");
 	var SpaceAsylumCaption = DialogFind(Player, "ChatRoomSpaceAsylum");
@@ -81,7 +107,7 @@ function FriendListLoadFriendList(data) {
 			FriendListContent += "<div class='FriendListTextColumn'>" + ((FriendListBeepLog[B].Sent) ? SentCaption : ReceivedCaption) + " " + TimerHourToString(FriendListBeepLog[B].Time) + "</div>";
 			FriendListContent += "</div>";
 		}
-		
+
 	// In Delete mode, we show the friend list and allow the user to remove them
 	if (FriendListMode[FriendListModeIndex] == "Delete")
 		for (var F = 0; F < data.length; F++)
@@ -99,7 +125,12 @@ function FriendListLoadFriendList(data) {
 
 }
 
-// When the user wants to delete someone from her friend list (must click twice to confirm deletion)
+/**
+ * When the user wants to delete someone from her friend list this must be confirmed. 
+ * This function either displays the confirm message or deletes the friend from the player's friendlist
+ * @param {number} MemberNumber - The member to delete from the friendlist
+ * @returns {void} - Nothing
+ */
 function FriendListDelete(MemberNumber) {
 	if (FriendListConfirmDelete.indexOf(MemberNumber) >= 0) {
 		Player.FriendList.splice(Player.FriendList.indexOf(MemberNumber), 1);
@@ -108,7 +139,11 @@ function FriendListDelete(MemberNumber) {
 	ServerSend("AccountQuery", { Query: "OnlineFriends" });
 }
 
-// When the user wants to beep someone
+/**
+ * Beeps a given member by sending the name and the current room of the beepee. Also adds an entry to the beep log of the player
+ * @param {number} MemberNumber - The ID of the player to beep
+ * @param {string} MemberName - The name of the player to beep
+ */
 function FriendListBeep(MemberNumber, MemberName) {
 	ServerSend("AccountBeep", { MemberNumber: MemberNumber });
 	FriendListBeepLog.push({ MemberNumber: MemberNumber, MemberName: MemberName, ChatRoomName: ((ChatRoomData == null) ? null : ChatRoomData.Name), Sent: true, Time: new Date() });
