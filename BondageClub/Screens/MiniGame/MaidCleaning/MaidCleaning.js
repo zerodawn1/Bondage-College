49205b5202a6c05a1736f2be6738d0414fcb2f50
ace-1331 <59637973+ace-1331@users.noreply.github.com>
2020-07-20 21:36:25 -0400

    jsdoc maid cleaning (#1131)
    Co-authored-by: Ace <>

--- BondageClub/Screens/MiniGame/MaidCleaning/MaidCleaning.js
+++ BondageClub/Screens/MiniGame/MaidCleaning/MaidCleaning.js
@@ -5,7 +5,11 @@ var MaidCleaningPlayerX = 0;
 var MaidCleaningPlayerY = 0;
 var MaidCleaningSpotSize = 100;
 
-// Generates all the spots to clean
+/**
+ * Generates all the spots to clean for the main cleaning minigame
+ * @param {number} MaxSpot - Maximum amount of spots to generate
+ * @returns {void} - Nothing
+ */
 function MaidCleaningGenerateSpots(MaxSpot) {
 
 	// Full the spots sequence
@@ -24,13 +28,19 @@ function MaidCleaningGenerateSpots(MaxSpot) {
 
 }
 
-// Draw the cleaning spots
+/**
+ * Draw the cleaning spots for the maid cleaning mini game
+ * @returns {void} - Nothing
+ */
 function MaidCleaningDrawSpots() {
 	for (var S = 0; S < MaidCleaningSpots.length; S++)
 		DrawImage("Screens/MiniGame/MaidCleaning/Spot" + MaidCleaningSpots[S].T + ".png", MaidCleaningSpots[S].X - (MaidCleaningSpotSize / 2), MaidCleaningSpots[S].Y - (MaidCleaningSpotSize / 2));
 }
 
-// Loads the maid cleaning mini game
+/**
+ * Loads the maid cleaning mini game. It will select a random background and generate spots based on the difficulty
+ * @returns {void} - Nothing
+ */
 function MaidCleaningLoad() {
 
 	// Picks a random place to clean
@@ -54,7 +64,10 @@ function MaidCleaningLoad() {
 
 }
 
-// Run the maid cleaning mini game
+/**
+ * Runs the maid cleaning mini game
+ * @returns {void} - Nothing
+ */
 function MaidCleaningRun() {
 
 	// The game ends in victory if everything is clean or in defeat if the time runs out
@@ -74,14 +87,21 @@ function MaidCleaningRun() {
 
 }
 
-// Ends the game and sends the result back to the screen
+/**
+ * Ends the game and sends the result back to the screen
+ * @param {boolean} Victory - Whether or not the game was won
+ * @returns {void} - Nothing
+ */
 function MaidCleaningEnd(Victory) {
 	MiniGameVictory = Victory;
 	MiniGameEnded = true;
 	MiniGameTimer = CommonTime();
 }
 
-// When the user clicks in the maid cleaning mini game
+/**
+ * Handles moving the player on the screen with a click, used for mobile users
+ * @returns {void} - Nothing
+ */
 function MaidCleaningDoMove() {
 
 	// If the position changed
@@ -105,7 +125,9 @@ function MaidCleaningDoMove() {
 
 }
 
-// On mobile, we need to move the player on click
+/**
+ * Handles clicks during the maid cleaning minigame. On mobile, we need to move the player on a click.
+ */
 function MaidCleaningClick() {
 	if (CommonIsMobile) MaidCleaningDoMove();
 }
