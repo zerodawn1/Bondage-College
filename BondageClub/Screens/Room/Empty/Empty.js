4977acbf231725ee410f194f399999e64c7251e0
ace-1331 <59637973+ace-1331@users.noreply.github.com>
2020-07-27 14:53:04 -0400

    jsdoc empty.js (#1183)
    Co-authored-by: Ace <>

--- BondageClub/Screens/Room/Empty/Empty.js
+++ BondageClub/Screens/Room/Empty/Empty.js
@@ -3,49 +3,86 @@ var EmptyBackground = "MainHall";
 var EmptyCharacter = [];
 var EmptyCharacterOffset = 0;
 
-// When used in struggle mode
+/**
+ * Checks if the player struggled successfully within the allowed time
+ * @returns {boolean} - Returns TRUE if the player managed to struggle out in time
+ */
 function EmptyStruggleSuccess() { return (!Player.IsRestrained() && Player.CanTalk() && (CurrentTime < ManagementTimer)) }
+/**
+ * Checks if the player has not managed to struggle out in time
+ * @returns {boolean} - Returns TRUE if the player has failed to struggle out.
+ */
 function EmptyStruggleFail() { return (CurrentTime >= ManagementTimer) }
+/**
+ * Checks if the player is in struggle mode.
+ * @returns {boolean} - Returns TRUE if the player still has time to try to struggle out.
+ */
 function EmptyStruggleProgress() { return ((Player.IsRestrained() || !Player.CanTalk()) && (CurrentTime < ManagementTimer)) }
+/**
+ * Checks if the player can do a bondage training.
+ * @returns {boolean} - Whether the player is ready to do a bondage training or not.
+ */
 function EmptySlaveMarketReadyForBondageTraining() { return (Player.CanInteract() && !CurrentCharacter.CanTalk() && CurrentCharacter.IsBlind() && !CurrentCharacter.CanInteract() && !CurrentCharacter.CanWalk()) }
 
-// Loads the empty room screen
+/**
+ * Loads the empty room screen. Does nothing, required for future use and because it is called dynamically when loading the screen.
+ * @returns {void} - Nothing
+ */
 function EmptyLoad() {
 }
 
-// Run the empty room screen
+/**
+ * Runs and draws the empty room screen, it will place each character inside the room evenly
+ * @returns {void} - Nothing
+ */
 function EmptyRun() {
 	for (var C = 0; C < EmptyCharacter.length; C++)
 		DrawCharacter(EmptyCharacter[C], 1000 - EmptyCharacter.length * 250 + C * 500 + EmptyCharacterOffset, 0, 1);
 }
 
-// When the user clicks in the empty room screen
+/**
+ * Handles clicks in the empty room screen
+ * @returns {void} - Nothing
+ */
 function EmptyClick() {
 	for (var C = 0; C < EmptyCharacter.length; C++)
 		if ((MouseX >= 1000 - EmptyCharacter.length * 250 + C * 500 + EmptyCharacterOffset) && (MouseX < 1500 - EmptyCharacter.length * 250 + C * 500 + EmptyCharacterOffset) && (MouseY >= 0) && (MouseY < 1000)) 
 			CharacterSetCurrent(EmptyCharacter[C]);
 }
 
-// Returns to the main hall
+/**
+ * Used to return the player to the main hall from the empty room when in the management room
+ * @returns {void} - Nothing
+ */
 function EmptyManagementMainHall() {
 	DialogLeave();
 	CommonSetScreen("Room", "MainHall");
 }
 
-// Locks the player in a cell for 5 minutes
+/**
+ * Triggered when management sends the player in a cell for 5 minutes with full restraints on
+ * @returns {void} - Nothing
+ */
 function EmptyManagementCell() {
 	DialogLeave();
 	CharacterFullRandomRestrain(Player, "ALL");
 	CellLock(5);
 }
 
-// Release the player from the item applied by the shop vendor
+/**
+ * Releases the player from the item applied by the shop vendor at the start of the job
+ * @returns {void} - Nothing
+ */
 function EmptyShopRelease() {
 	InventoryRemove(Player, ShopDemoItemGroup);
 	DialogChangeReputation("Dominant", -1);
 }
 
-// Release the player from the item applied by the shop vendor
+/**
+ * Releases the player from the item she was trying to sell, and sets the following dialog.
+ * @param {string} Sold - Whether or not the item was sold, "true" if sold
+ * @returns {void} - Nothing
+ */
 function EmptyShopEnd(Sold) {
 	Sold = (Sold == "true");
 	ShopVendor.Stage = (Sold) ? "33" : "34";
@@ -58,12 +95,20 @@ function EmptyShopEnd(Sold) {
 	CharacterRandomName(ShopCustomer)
 }
 
-// When the slave market training starts
+/**
+ * Starts the slave training
+ * @param {number} TrainingType - The type of training to do
+ * @returns {void} - Nothing
+ */
 function EmptySlaveMarketTrainingStart(TrainingType) {
 	CurrentCharacter.CurrentTraining = parseInt(TrainingType);
 }
 
-// Returns TRUE if the intensity level matches the level to test
+/**
+ * Checks if the intensity of the training is on par with the required level
+ * @param {string} TestLevel - Required level of intensity
+ * @returns {boolean} - Returns TRUE if the intensity level matches the level to test
+ */
 function EmptySlaveMarketTrainingLevelIs(TestLevel) {
 	if ((TestLevel == "Lowest") && (CurrentCharacter.TrainingIntensity <= 1)) return true;
 	if ((TestLevel == "Low") && (CurrentCharacter.TrainingIntensity >= 2) && (CurrentCharacter.TrainingIntensity <= 3)) return true;
@@ -73,7 +118,11 @@ function EmptySlaveMarketTrainingLevelIs(TestLevel) {
 	return false;
 }
 
-// When the training progresses
+/**
+ * Handles progressing through the slave market training
+ * @param {number} Intensity - Intensity of the activity performed
+ * @returns {void} - Nothing
+ */
 function EmptySlaveMarketTrainingProgress(Intensity) {
 	
 	// Intensity of activity gets added minus from 0 to 3 of random decay
@@ -98,7 +147,11 @@ function EmptySlaveMarketTrainingProgress(Intensity) {
 
 }
 
-// When the slave market training ends
+/**
+ * Triggered when the slave market training ends
+ * @param {string} Status - The status the game ended with, "Success" if won
+ * @returns {void} - Nothing
+ */
 function EmptySlaveMarketTrainingEnd(Status) {
 	var Money = CurrentCharacter.TrainingCountPerfect * 3;
 	DialogLeave();
