dfdc6f36d2d86e332b04411fe745b7418e3f3462
ace-1331 <59637973+ace-1331@users.noreply.github.com>
2020-08-25 15:58:53 -0400

    Fix: enclosed activities (#1322)
    * Removed unused property
    
    * Revert "Removed unused property"
    
    This reverts commit f1eec5d908ca4fe536a045a10d710115daa0edd4.
    
    * enclosed activities fix
    
    Co-authored-by: Ace <>

--- BondageClub/Scripts/Dialog.js
+++ BondageClub/Scripts/Dialog.js
@@ -537,7 +537,7 @@ function DialogMenuButtonBuild(C) {
 		if ((Player.CanInteract()) || DialogAlwaysAllowRestraint()) DialogMenuButton.push("ColorPick");
 
 		// Make sure the target player zone is allowed for an activity
-		if ((C.FocusGroup.Activity != null) && (!C.IsEnclose() || C.ID == 0) && ActivityAllowed() && (C.ArousalSettings != null) && (C.ArousalSettings.Zone != null) && (C.ArousalSettings.Active != null) && (C.ArousalSettings.Active != "Inactive"))
+		if ((C.FocusGroup.Activity != null) && ((!C.IsEnclose() && !Player.IsEnclose()) || C.ID == 0) && ActivityAllowed() && (C.ArousalSettings != null) && (C.ArousalSettings.Zone != null) && (C.ArousalSettings.Active != null) && (C.ArousalSettings.Active != "Inactive"))
 			for (let Z = 0; Z < C.ArousalSettings.Zone.length; Z++)
 				if ((C.ArousalSettings.Zone[Z].Name == C.FocusGroup.Name) && (C.ArousalSettings.Zone[Z].Factor != null) && (C.ArousalSettings.Zone[Z].Factor > 0)) {
 					ActivityDialogBuild(C);
