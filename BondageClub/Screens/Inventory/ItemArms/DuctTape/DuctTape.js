271db8ed3b8782098c12c6289980b7d491a43781
SandrineBC <61221552+SandrineBC@users.noreply.github.com>
2020-09-10 03:51:33 +0200

    Refactor: Duct tape on arms to use ExtendedItem (#1370)
    * Refactored duct tape on arms to extended items
    
    Co-authored-by: Nina-1474 <62667047+Nina-1474@users.noreply.github.com>

--- BondageClub/Screens/Inventory/ItemArms/DuctTape/DuctTape.js
+++ BondageClub/Screens/Inventory/ItemArms/DuctTape/DuctTape.js
@@ -1,79 +1,117 @@
 "use strict";
 var InventoryItemArmsDuctTapeMessage = "SelectTapeWrapping";
 
-// Loads the item extension properties
+const InventoryItemArmsDuctTapeOptions = [
+	{
+		Name: "Arms",
+		Property: {Type: null, Difficulty: 1},
+	},
+	{
+		Name: "Bottom",
+		SelfBondageLevel: 4,
+		Property: {
+			Type: "Bottom",
+			SetPose: ["BackElbowTouch"],
+			Block: ["ItemVulva", "ItemButt", "ItemPelvis", "ItemVulvaPiercings"],
+			Difficulty: 2,
+		},
+	},
+	{
+		Name: "Top",
+		Property: {
+			Type: "Top",
+			SetPose: ["BackElbowTouch"],
+			Block: ["ItemTorso", "ItemBreast", "ItemNipples", "ItemNipplesPiercings"],
+			Difficulty: 4,
+		},
+	},
+	{
+		Name: "Full",
+		Property: {
+			Type: "Full",
+			SetPose: ["BackElbowTouch"],
+			Block: ["ItemVulva", "ItemButt", "ItemPelvis", "ItemTorso", "ItemBreast", "ItemNipples", "ItemVulvaPiercings", "ItemNipplesPiercings"],
+			Difficulty: 6,
+		}
+	},
+	{
+		Name: "Complete",
+		Property: {
+			Type: "Complete",
+			SetPose: ["BackElbowTouch"],
+			Block: ["ItemVulva", "ItemButt", "ItemPelvis", "ItemTorso", "ItemBreast", "ItemNipples", "ItemVulvaPiercings", "ItemNipplesPiercings"],
+			Difficulty: 7,
+		}
+	},
+
+];
+
+/**
+ * Loads the item extension properties. Does nothing but propagate the call to the extended items
+ * @returns {void} - Nothing
+ */
 function InventoryItemArmsDuctTapeLoad() {
-	InventoryItemArmsDuctTapeMessage = "SelectTapeWrapping";
+	ExtendedItemLoad(InventoryItemArmsDuctTapeOptions, InventoryItemArmsDuctTapeMessage);
 }
 
-// Draw the item extension screen
+/**
+ * Draw the item extension screen. Does nothing but propagate the call to the extended items
+ * @returns {void} - Nothing
+ */
 function InventoryItemArmsDuctTapeDraw() {
-	
-	// Draw the header and item
-	DrawRect(1387, 125, 225, 275, "white");
-	DrawImageResize("Assets/" + DialogFocusItem.Asset.Group.Family + "/" + DialogFocusItem.Asset.Group.Name + "/Preview/" + DialogFocusItem.Asset.Name + ".png", 1389, 127, 221, 221);
-	DrawTextFit(DialogFocusItem.Asset.Description, 1500, 375, 221, "black");
-
-	// Draw the possible poses
-	DrawText(DialogFind(Player, InventoryItemArmsDuctTapeMessage), 1500, 50, "white", "gray");
-	DrawButton(1000, 450, 225, 225, "", (DialogFocusItem.Property == null) ? "#888888" : "White");
-	DrawImage("Screens/Inventory/" + DialogFocusItem.Asset.Group.Name + "/" + DialogFocusItem.Asset.Name + "/Arms.png", 1000, 449);
-	DrawText(DialogFind(Player, "DuctTapePoseArms"), 1113, 425, "white", "gray");
-	DrawButton(1375, 450, 225, 225, "", ((DialogFocusItem.Property != null) && (DialogFocusItem.Property.Restrain == "Bottom")) ? "#888888" : "White");
-	DrawImage("Screens/Inventory/" + DialogFocusItem.Asset.Group.Name + "/" + DialogFocusItem.Asset.Name + "/Bottom.png", 1375, 449);
-	DrawText(DialogFind(Player, "DuctTapePoseBottom"), 1488, 425, "white", "gray");
-	DrawButton(1750, 450, 225, 225, "", ((DialogFocusItem.Property != null) && (DialogFocusItem.Property.Restrain == "Top")) ? "#888888" : "White");
-	DrawImage("Screens/Inventory/" + DialogFocusItem.Asset.Group.Name + "/" + DialogFocusItem.Asset.Name + "/Top.png", 1750, 449);
-	DrawText(DialogFind(Player, "DuctTapePoseTop"), 1863, 425, "white", "gray");
-	DrawButton(1150, 750, 225, 225, "", ((DialogFocusItem.Property != null) && (DialogFocusItem.Property.Restrain == "Full")) ? "#888888" : "White");
-	DrawImage("Screens/Inventory/" + DialogFocusItem.Asset.Group.Name + "/" + DialogFocusItem.Asset.Name + "/Full.png", 1150, 749);
-	DrawText(DialogFind(Player, "DuctTapePoseFull"), 1263, 725, "white", "gray");
-	DrawButton(1600, 750, 225, 225, "", ((DialogFocusItem.Property != null) && (DialogFocusItem.Property.Restrain == "Complete")) ? "#888888" : "White");
-	DrawImage("Screens/Inventory/" + DialogFocusItem.Asset.Group.Name + "/" + DialogFocusItem.Asset.Name + "/Complete.png", 1600, 749);
-	DrawText(DialogFind(Player, "DuctTapePoseComplete"), 1713, 725, "white", "gray");
-	
+	ExtendedItemDraw(InventoryItemArmsDuctTapeOptions, "DuctTapePose");
 }
 
-// Catches the item extension clicks
+/**
+ * Catches the item extension clicks. Does nothing but propagate the call to the extended items
+ * @returns {void} - Nothing
+ */
 function InventoryItemArmsDuctTapeClick() {
-	if ((MouseX >= 1885) && (MouseX <= 1975) && (MouseY >= 25) && (MouseY <= 110)) DialogFocusItem = null;
-	if ((MouseX >= 1000) && (MouseX <= 1225) && (MouseY >= 450) && (MouseY <= 675) && (DialogFocusItem.Property != null)) InventoryItemArmsDuctTapeSetPose(null);
-	if ((MouseX >= 1375) && (MouseX <= 1600) && (MouseY >= 450) && (MouseY <= 675) && ((DialogFocusItem.Property == null) || (DialogFocusItem.Property.Restrain != "Bottom"))) InventoryItemArmsDuctTapeSetPose("Bottom");
-	if ((MouseX >= 1750) && (MouseX <= 1975) && (MouseY >= 450) && (MouseY <= 675) && ((DialogFocusItem.Property == null) || (DialogFocusItem.Property.Restrain != "Top"))) InventoryItemArmsDuctTapeSetPose("Top");
-	if ((MouseX >= 1150) && (MouseX <= 1375) && (MouseY >= 750) && (MouseY <= 975) && ((DialogFocusItem.Property == null) || (DialogFocusItem.Property.Restrain != "Full"))) InventoryItemArmsDuctTapeSetPose("Full");
-	if ((MouseX >= 1600) && (MouseX <= 1825) && (MouseY >= 750) && (MouseY <= 975) && ((DialogFocusItem.Property == null) || (DialogFocusItem.Property.Restrain != "Complete"))) InventoryItemArmsDuctTapeSetPose("Complete");
+	ExtendedItemClick(InventoryItemArmsDuctTapeOptions);
 }
 
-// Sets the duct tape type (the wraps require no clothes)
-function InventoryItemArmsDuctTapeSetPose(NewPose) {
-	var C = (Player.FocusGroup != null) ? Player : CurrentCharacter;
-	if ((NewPose == null) || ((InventoryGet(C, "Cloth") == null) && (InventoryGet(C, "ClothLower") == null))) {
-		if (CurrentScreen == "ChatRoom") {
-			DialogFocusItem = InventoryGet(C, C.FocusGroup.Name);
-			InventoryItemArmsDuctTapeLoad();
-		}
-		if (NewPose == null) delete DialogFocusItem.Property;
-		else {
-			DialogFocusItem.Property = {SetPose: ["BackElbowTouch"], Type: NewPose, Hide: ["Cloth", "ClothLower"]};
-			if (NewPose == "Bottom") DialogFocusItem.Property.Block = ["ItemVulva", "ItemButt", "ItemPelvis"];
-			if (NewPose == "Top") DialogFocusItem.Property.Block = ["ItemTorso", "ItemBreast", "ItemNipples"];
-			if (NewPose == "Full") DialogFocusItem.Property.Block = ["ItemVulva", "ItemButt", "ItemPelvis", "ItemTorso", "ItemBreast", "ItemNipples"];
-			if (NewPose == "Complete") DialogFocusItem.Property.Block = ["ItemVulva", "ItemButt", "ItemPelvis", "ItemTorso", "ItemBreast", "ItemNipples"];
-			if (NewPose == "Bottom") DialogFocusItem.Property.Difficulty = 2;
-			if (NewPose == "Top") DialogFocusItem.Property.Difficulty = 4;
-			if (NewPose == "Full") DialogFocusItem.Property.Difficulty = 6;
-			if (NewPose == "Complete") DialogFocusItem.Property.Difficulty = 7;
-		}
-		CharacterRefresh(C);
-		var msg = "DuctTapeRestrain" + ((NewPose == null) ? "Hands" : NewPose);
-		var Dictionary = [];
-		Dictionary.push({Tag: "SourceCharacter", Text: Player.Name, MemberNumber: Player.MemberNumber});
-		Dictionary.push({Tag: "DestinationCharacter", Text: C.Name, MemberNumber: C.MemberNumber});
-		Dictionary.push({Tag: "TargetCharacter", Text: C.Name, MemberNumber: C.MemberNumber});
-		ChatRoomPublishCustomAction(msg, true, Dictionary);
-		if (DialogInventory != null) {
-			DialogFocusItem = null;
-			DialogMenuButtonBuild(C);
-		}
-	} else InventoryItemArmsDuctTapeMessage = "RemoveClothesForItem";
+/**
+ * Publishes the action to the chat room
+ * @param {Character} C - The character who get restrained
+ * @param {Option} Option - The extended item option
+ * @param {string} Option.Name - The name of the option. Used for finding the right dialog entry
+ * @param {Option} PreviousOption - The extended item option that was previously used. Not used in this item
+ * @returns {void} - Nothing
+ */
+function InventoryItemArmsDuctTapePublishAction(C, Option, PreviousOption) {
+	var msg = "DuctTapeRestrain" + Option.Name;
+	var Dictionary = [
+		{ Tag: "SourceCharacter", Text: Player.Name, MemberNumber: Player.MemberNumber },
+		{ Tag: "TargetCharacter", Text: C.Name, MemberNumber: C.MemberNumber },
+	];
+	ChatRoomPublishCustomAction(msg, true, Dictionary);
+}
+
+/**
+ * The NPC dialog is for what the NPC says to you when you make a change to their restraints - the dialog lookup is on a 
+ * per-NPC basis. You basically put the "AssetName" + OptionName in there to allow individual NPCs to override their default 
+ * "GroupName" dialog if for example we ever wanted an NPC to react specifically to having the restraint put on them. 
+ * That could be done by adding an "AssetName" entry (or entries) to that NPC's dialog CSV
+ * @param {Character} C - The NPC to whom the restraint is applied
+ * @param {Option} Option - The chosen option for this extended item
+ * @returns {void} - Nothing
+ */
+function InventoryItemArmsDuctTapeNpcDialog(C, Option) {
+	C.CurrentDialog = DialogFind(C, "ItemArmsDuctTape" + Option.Name, "ItemArms");
+}
+
+/**
+ * Validate function that checks, if the restrained character wears outer clothes. If so, the duct tape cannot be applied
+ * @param {Option} Option - The option to be applied on the character. Not used in this item
+ * @returns 
+ */
+function InventoryItemArmsDuctTapeValidate(Option) {
+	var C = CharacterGetCurrent();
+	var Allowed = true;
+
+	if (InventoryGet(C, "Cloth") || InventoryGet(C, "ClothLower")) {
+		DialogExtendedMessage = DialogFind(Player, "RemoveClothesForItem", "ItemArms");
+		Allowed = false;
+	}
+	return Allowed;
 }
