040dc004c7145bd28d7b112501dc9220f9a919f3
Ace <>
2020-09-10 21:48:06 -0400

    Fix actions

--- BondageClub/Screens/Inventory/ItemPelvis/PolishedChastityBelt/PolishedChastityBelt.js
+++ BondageClub/Screens/Inventory/ItemPelvis/PolishedChastityBelt/PolishedChastityBelt.js
@@ -2,3 +2,5 @@
 function InventoryItemPelvisPolishedChastityBeltLoad() { InventoryItemPelvisMetalChastityBeltLoad(); }
 function InventoryItemPelvisPolishedChastityBeltDraw() { InventoryItemPelvisMetalChastityBeltDraw(); }
 function InventoryItemPelvisPolishedChastityBeltClick() { InventoryItemPelvisMetalChastityBeltClick(); }
+function InventoryItemPelvisPolishedChastityBeltPublishAction(C, Option) { InventoryItemPelvisMetalChastityBeltPublishAction(C, Option); }
+function InventoryItemPelvisPolishedChastityBeltNpcDialog(C, Option) { InventoryItemPelvisMetalChastityBeltNpcDialog(C, Option); }
