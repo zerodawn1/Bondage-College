e7968c9e2916547829d18be7b8b5bde3a2ba0356
ace-1331 <59637973+ace-1331@users.noreply.github.com>
2020-09-11 21:27:26 -0400

    Fix: Rope conversion. (#1401)
    * fix nylon rope
    
    * Update NylonRopeHarness.js
    
    Co-authored-by: Ace <>

--- BondageClub/Screens/Inventory/ItemTorso/NylonRopeHarness/NylonRopeHarness.js
+++ BondageClub/Screens/Inventory/ItemTorso/NylonRopeHarness/NylonRopeHarness.js
@@ -4,3 +4,5 @@
 function InventoryItemTorsoNylonRopeHarnessLoad() { InventoryItemTorsoHempRopeHarnessLoad(); }
 function InventoryItemTorsoNylonRopeHarnessDraw() { InventoryItemTorsoHempRopeHarnessDraw(); }
 function InventoryItemTorsoNylonRopeHarnessClick() { InventoryItemTorsoHempRopeHarnessClick(); }
+function InventoryItemTorsoNylonRopeHarnessPublishAction(C, Option) { InventoryItemTorsoHempRopeHarnessPublishAction(C, Option); }
+function InventoryItemTorsoNylonRopeHarnessNpcDialog(C, Option) {  InventoryItemTorsoHempRopeHarnessNpcDialog(C, Option); }
