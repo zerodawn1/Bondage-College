e5702642b049d7d13bbee73029726860973ac2de
ace-1331 <59637973+ace-1331@users.noreply.github.com>
2020-09-11 21:10:02 -0400

    Plug gag refactor (#1403)

--- BondageClub/Screens/Inventory/ItemMouth/PlugGag/PlugGag.js
+++ BondageClub/Screens/Inventory/ItemMouth/PlugGag/PlugGag.js
@@ -1,69 +1,46 @@
 "use strict";
 
+var InventoryItemMouthPlugGagOptions = [
+	{
+		Name: "Open",
+		Property: {
+			Type: null,
+			Effect: ["GagMedium", "OpenMouth"],
+		},
+	},
+	{
+		Name: "Plug",
+		Property: {
+			Type: "Plug",
+			Effect: ["BlockMouth", "GagTotal"],
+		},
+	},
+];
+
 // Loads the item extension properties
 function InventoryItemMouthPlugGagLoad() {
-	if (DialogFocusItem.Property == null) {
-		DialogFocusItem.Property = { Type: null };
-	}
-	if (DialogFocusItem.Property.Effect == null) {
-		DialogFocusItem.Property.Effect = ["GagMedium", "OpenMouth"];
-		var C = (Player.FocusGroup != null) ? Player : CurrentCharacter;
-		CharacterRefresh(C);
-		ChatRoomCharacterItemUpdate(C, DialogFocusItem.Asset.Group.Name);
-	}
+	ExtendedItemLoad(InventoryItemMouthPlugGagOptions, "SelectGagType");
 }
 
 // Draw the item extension screen
 function InventoryItemMouthPlugGagDraw() {
-
-	// Draw the header and item
-	DrawRect(1387, 125, 225, 275, "white");
-	DrawImageResize("Assets/" + DialogFocusItem.Asset.Group.Family + "/" + DialogFocusItem.Asset.Group.Name + "/Preview/" + DialogFocusItem.Asset.Name + ".png", 1389, 127, 221, 221);
-	DrawTextFit(DialogFocusItem.Asset.Description, 1500, 375, 221, "black");
-
-	// Draw the possible gag types
-	DrawText(DialogFind(Player, "SelectGagType"), 1500, 500, "white", "gray");
-	DrawButton(1175, 550, 225, 225, "", (DialogFocusItem.Property.Type == null || DialogFocusItem.Property.Type == "Open") ? "#888888" : "White");
-	DrawImage("Screens/Inventory/" + DialogFocusItem.Asset.Group.Name + "/" + DialogFocusItem.Asset.Name + "/Open.png", 1175, 549);
-	DrawText(DialogFind(Player, "PlugGagMouthTypeOpen"), 1288, 800, "white", "gray");
-	DrawButton(1600, 550, 225, 225, "", ((DialogFocusItem.Property.Type != null) && (DialogFocusItem.Property.Type == "Plug")) ? "#888888" : "White");
-	DrawImage("Screens/Inventory/" + DialogFocusItem.Asset.Group.Name + "/" + DialogFocusItem.Asset.Name + "/Plug.png", 1600, 549);
-	DrawText(DialogFind(Player, "PlugGagMouthTypePlug"), 1713, 800, "white", "gray");
+	ExtendedItemDraw(InventoryItemMouthPlugGagOptions, "PlugGagMouthType");
 }
 
 // Catches the item extension clicks
 function InventoryItemMouthPlugGagClick() {
-	if ((MouseX >= 1885) && (MouseX <= 1975) && (MouseY >= 25) && (MouseY <= 110)) DialogFocusItem = null;
-	if ((MouseX >= 1175) && (MouseX <= 1400) && (MouseY >= 550) && (MouseY <= 775) && (DialogFocusItem.Property.Type != null)) InventoryItemMouthPlugGagSetType(null);
-	if ((MouseX >= 1600) && (MouseX <= 1825) && (MouseY >= 550) && (MouseY <= 775) && ((DialogFocusItem.Property.Type == null) || (DialogFocusItem.Property.Type != "Plug"))) InventoryItemMouthPlugGagSetType("Plug");
+	ExtendedItemClick(InventoryItemMouthPlugGagOptions);
 }
 
-// Sets the gag type (Plug, Open)
-function InventoryItemMouthPlugGagSetType(NewType) {
-	var C = (Player.FocusGroup != null) ? Player : CurrentCharacter;
-	if (CurrentScreen == "ChatRoom") {
-		DialogFocusItem = InventoryGet(C, C.FocusGroup.Name);
-		InventoryItemMouthPlugGagLoad();
-	}
-	DialogFocusItem.Property.Type = NewType;
-	if (NewType == null) DialogFocusItem.Property.Effect = ["GagMedium", "OpenMouth"];
-	else if (NewType == "Plug") DialogFocusItem.Property.Effect = ["BlockMouth", "GagTotal"];
-
-	// Adds the lock effect back if it was padlocked
-	if ((DialogFocusItem.Property.LockedBy != null) && (DialogFocusItem.Property.LockedBy != "")) {
-		if (DialogFocusItem.Property.Effect == null) DialogFocusItem.Property.Effect = [];
-		DialogFocusItem.Property.Effect.push("Lock");
-	}
-
-	// Refreshes the character and chatroom
-	CharacterRefresh(C);
-	var msg = "PlugGagMouthSet" + ((NewType) ? NewType : "Open");
-	var Dictionary = [];
-	Dictionary.push({Tag: "SourceCharacter", Text: Player.Name, MemberNumber: Player.MemberNumber});
-	Dictionary.push({Tag: "DestinationCharacter", Text: C.Name, MemberNumber: C.MemberNumber});
+function InventoryItemMouthPlugGagPublishAction(C, Option) {
+	var msg = "PlugGagMouthSet" + Option.Name;
+	var Dictionary = [
+		{ Tag: "SourceCharacter", Text: Player.Name, MemberNumber: Player.MemberNumber },
+		{ Tag: "DestinationCharacter", Text: C.Name, MemberNumber: C.MemberNumber },
+	];
 	ChatRoomPublishCustomAction(msg, true, Dictionary);
-	if (DialogInventory != null) {
-		DialogFocusItem = null;
-		DialogMenuButtonBuild(C);
-	}
+}
+
+function InventoryItemMouthPlugGagNpcDialog(C, Option) {
+	C.CurrentDialog = DialogFind(C, "ItemMouthPlugGag" + Option.Name, "ItemMouth");
 }
