19e027c9837c4c9c4b36c5dd2a7b5b8259c3fbf1
SandrineBC <61221552+SandrineBC@users.noreply.github.com>
2020-09-23 21:40:07 +0200

    Refactor: Lactation pump to extended items (#1440)

--- BondageClub/Screens/Inventory/ItemNipples/LactationPump/LactationPump.js
+++ BondageClub/Screens/Inventory/ItemNipples/LactationPump/LactationPump.js
@@ -1,47 +1,95 @@
 "use strict";
 
-// Loads the item extension properties
+var InventoryItemNipplesLactationPumpOptions = [
+	{
+		Name: "Off",
+		Property: {
+			Type: null,
+			SuctionLevel: "0",
+		},
+	},
+	{
+		Name: "LowSuction",
+		Property: {
+			Type: "LowSuction",
+			SuctionLevel: "1",
+		},
+	},
+	{
+		Name: "MediumSuction",
+		Property: {
+			Type: "MediumSuction",
+			SuctionLevel: "2",
+		},
+	},
+	{
+		Name: "HighSuction",
+		Property: {
+			Type: "HighSuction",
+			SuctionLevel: "3",
+		},
+	},
+	{
+		Name: "MaximumSuction",
+		Property: {
+			Type: "MaximumSuction",
+			SuctionLevel: "4",
+		},
+	},
+];
+
+/**
+ * Loads the item extension properties
+ * @returns {void} - Nothing
+ */
 function InventoryItemNipplesLactationPumpLoad() {
-	if (DialogFocusItem.Property == null) DialogFocusItem.Property = { SuctionLevel: 0 };
-	if (DialogFocusItem.Property.SuctionLevel == null) DialogFocusItem.Property.SuctionLevel = 0;
+	ExtendedItemLoad(InventoryItemNipplesLactationPumpOptions, "LactationPumpSelectSetting");
 }
 
-// Draw the item extension screen
+/**
+ * Draw the item extension screen
+ * @returns {void} - Nothing
+ */
 function InventoryItemNipplesLactationPumpDraw() {
-	DrawRect(1387, 225, 225, 275, "white");
-	DrawImageResize("Assets/" + DialogFocusItem.Asset.Group.Family + "/" + DialogFocusItem.Asset.Group.Name + "/Preview/" + DialogFocusItem.Asset.Name + ".png", 1389, 227, 221, 221);
-	DrawTextFit(DialogFocusItem.Asset.Description, 1500, 475, 221, "black");
-	DrawText(DialogFind(Player, "LactationPumpSuctionPower" + DialogFocusItem.Property.SuctionLevel.toString()), 1500, 600, "White", "Gray");
-	if (DialogFocusItem.Property.SuctionLevel > 0) DrawButton(1200, 650, 200, 55, DialogFind(Player, "LactationPumpoff"), "White");
-	if (DialogFocusItem.Property.SuctionLevel != 1) DrawButton(1550, 650, 200, 55, DialogFind(Player, "LactationPumpLowSuction"), "White");
-	if (DialogFocusItem.Property.SuctionLevel != 2) DrawButton(1200, 710, 200, 55, DialogFind(Player, "LactationPumpMediumSuction"), "White");
-	if (DialogFocusItem.Property.SuctionLevel != 3) DrawButton(1550, 710, 200, 55, DialogFind(Player, "LactationPumpHighSuction"), "White");
-	if (DialogFocusItem.Property.SuctionLevel < 4) DrawButton(1375, 770, 200, 55, DialogFind(Player, "LactationPumpMaximumSuction"), "White");
+	ExtendedItemDraw(InventoryItemNipplesLactationPumpOptions, "LactationPump", InventoryItemNipplesLactationPumpOptions.length, false);
 }
 
-// Catches the item extension clicks
+/**
+ * Catches the item extension clicks
+ * @returns {void} - Nothing
+ */
 function InventoryItemNipplesLactationPumpClick() {
-	if ((MouseX >= 1885) && (MouseX <= 1975) && (MouseY >= 25) && (MouseY <= 110)) DialogFocusItem = null;
-	if ((MouseX >= 1200) && (MouseX <= 1400) && (MouseY >= 650) && (MouseY <= 705) && (DialogFocusItem.Property.SuctionLevel > 0)) InventoryItemNipplesLactationPumpIntensity(0 - DialogFocusItem.Property.SuctionLevel);
-	if ((MouseX >= 1550) && (MouseX <= 1750) && (MouseY >= 650) && (MouseY <= 705) && (DialogFocusItem.Property.SuctionLevel != 1)) InventoryItemNipplesLactationPumpIntensity(1 - DialogFocusItem.Property.SuctionLevel);
-	if ((MouseX >= 1200) && (MouseX <= 1400) && (MouseY >= 710) && (MouseY <= 765) && (DialogFocusItem.Property.SuctionLevel != 2)) InventoryItemNipplesLactationPumpIntensity(2 - DialogFocusItem.Property.SuctionLevel);
-	if ((MouseX >= 1550) && (MouseX <= 1750) && (MouseY >= 710) && (MouseY <= 765) && (DialogFocusItem.Property.SuctionLevel != 3)) InventoryItemNipplesLactationPumpIntensity(3 - DialogFocusItem.Property.SuctionLevel);
-	if ((MouseX >= 1375) && (MouseX <= 1575) && (MouseY >= 770) && (MouseY <= 825) && (DialogFocusItem.Property.SuctionLevel < 4)) InventoryItemNipplesLactationPumpIntensity(4 - DialogFocusItem.Property.SuctionLevel);
+	ExtendedItemClick(InventoryItemNipplesLactationPumpOptions, false, InventoryItemNipplesLactationPumpOptions.length, false);
 }
 
-// Sets the Nipple Suction Cups Level
-function InventoryItemNipplesLactationPumpIntensity(Modifier) {
-	var C = (Player.FocusGroup != null) ? Player : CurrentCharacter;
-	DialogFocusItem.Property.SuctionLevel = DialogFocusItem.Property.SuctionLevel + Modifier;
-	if (DialogFocusItem.Property.SuctionLevel == 0);
-	if (DialogFocusItem.Property.SuctionLevel == 1);
-	if (DialogFocusItem.Property.SuctionLevel == 2);
-	if (DialogFocusItem.Property.SuctionLevel == 3);
-	if (DialogFocusItem.Property.SuctionLevel == 4);
-	if (C.ID == 0) ServerPlayerAppearanceSync();
-	var Dictionary = [];
-	Dictionary.push({Tag: "DestinationCharacter", Text: C.Name, MemberNumber: C.MemberNumber});
-	Dictionary.push({Tag: "SourceCharacter", Text: Player.Name, MemberNumber: Player.MemberNumber});
-    ChatRoomPublishCustomAction("NipPow" + ((Modifier > 0) ? "tightens" : "loosens") + "To" + DialogFocusItem.Property.SuctionLevel, true, Dictionary);
-} 
+/**
+ * Publishes the message to the chat
+ * @param {Character} C - The target character
+ * @param {Option} Option - The currently selected Option
+ * @param {Option} PreviousOption - The previously selected Option
+ * @returns {void} - Nothing
+ */
+function InventoryItemNipplesLactationPumpPublishAction(C, Option, PreviousOption) {
+	var NewIndex = InventoryItemNipplesLactationPumpOptions.indexOf(Option);
+	var PreviousIndex = InventoryItemNipplesLactationPumpOptions.indexOf(PreviousOption);
+	var msg = "LactationPumpPower" + ((NewIndex > PreviousIndex) ? "tightens" : "loosens") + "To" + Option.Property.SuctionLevel;
+	var Dictionary = [
+		{ Tag: "SourceCharacter", Text: Player.Name, MemberNumber: Player.MemberNumber },
+		{ Tag: "DestinationCharacter", Text: C.Name, MemberNumber: C.MemberNumber },
+	];
+	ChatRoomPublishCustomAction(msg, true, Dictionary);
+}
+
+/**
+ * The NPC dialog is for what the NPC says to you when you make a change to their restraints - the dialog lookup is on a 
+ * per-NPC basis. You basically put the "AssetName" + OptionName in there to allow individual NPCs to override their default 
+ * "GroupName" dialog if for example we ever wanted an NPC to react specifically to having the restraint put on them. 
+ * That could be done by adding an "AssetName" entry (or entries) to that NPC's dialog CSV
+ * @param {Character} C - The NPC to whom the restraint is applied
+ * @param {Option} Option - The chosen option for this extended item
+ * @returns {void} - Nothing
+ */
+function InventoryItemNipplesLactationPumpNpcDialog(C, Option) {
+	C.CurrentDialog = DialogFind(C, "InventoryItemNipplesLactationPumpNPCReaction" + Option.Name, "ItemNipples");
+}
 
