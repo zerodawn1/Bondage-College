f7e3d720fb47319502d351e0142e38640a3cb7af
Ben987 <30805159+Ben987@users.noreply.github.com>
2020-09-28 22:56:17 -0400

    Movie Studio - Intro Script
    Movie Studio - Intro Script

--- BondageClub/Screens/Room/MovieStudio/MovieStudio.js
+++ BondageClub/Screens/Room/MovieStudio/MovieStudio.js
@@ -2,6 +2,11 @@
 var MovieStudioBackground = "MovieStudio";
 var MovieStudioDirectory = null;
 var MovieStudioCurrentMovie = "";
+var MovieStudioCurrentScene = "";
+var MovieStudioCurrentRole = "";
+var MovieStudioActor1 = null;
+var MovieStudioActor2 = null;
+var MovieStudioTimer = null;
 
 /**
  * Loads the Movie Studio introduction room screen
@@ -25,9 +30,16 @@ function MovieStudioRun() {
 	if (MovieStudioCurrentMovie == "") {
 		DrawCharacter(Player, 500, 0, 1);
 		DrawCharacter(MovieStudioDirectory, 1000, 0, 1);
+		if (Player.CanWalk()) DrawButton(1885, 25, 90, 90, "", "White", "Icons/Exit.png", TextGet("Leave"));
+		DrawButton(1885, 145, 90, 90, "", "White", "Icons/Character.png", TextGet("Profile"));
 	}
-	if (Player.CanWalk()) DrawButton(1885, 25, 90, 90, "", "White", "Icons/Exit.png", TextGet("Leave"));
-	DrawButton(1885, 145, 90, 90, "", "White", "Icons/Character.png", TextGet("Profile"));
+	if ((MovieStudioCurrentMovie == "Interview") && (MovieStudioCurrentScene == "1")) {
+		DrawCharacter(MovieStudioActor1, 250, 0, 1);
+		DrawCharacter(Player, 750, 0, 1);
+		DrawCharacter(MovieStudioActor2, 1250, 0, 1);
+		DrawText(TextGet("Recording"), 1900, 920, "#FF4444", "White");
+		DrawText(TimermsToTime(CurrentTime - MovieStudioTimer), 1900, 960, "#FF4444", "White");
+	}
 }
 
 /**
@@ -37,6 +49,45 @@ function MovieStudioRun() {
 function MovieStudioClick() {
 	if ((MovieStudioCurrentMovie == "") && MouseIn(500, 0, 500, 1000)) CharacterSetCurrent(Player);
 	if ((MovieStudioCurrentMovie == "") && MouseIn(1000, 0, 500, 1000)) CharacterSetCurrent(MovieStudioDirectory);
-	if (MouseIn(1885, 25, 90, 90) && Player.CanWalk()) CommonSetScreen("Room", "MainHall");
-	if (MouseIn(1885, 145, 90, 90)) InformationSheetLoadCharacter(Player);
+	if ((MovieStudioCurrentMovie == "") && MouseIn(1885, 25, 90, 90) && Player.CanWalk()) CommonSetScreen("Room", "MainHall");
+	if ((MovieStudioCurrentMovie == "") && MouseIn(1885, 145, 90, 90)) InformationSheetLoadCharacter(Player);
+	if ((MovieStudioCurrentMovie == "Interview") && (MovieStudioCurrentScene == "1") && MouseIn(250, 0, 500, 1000)) CharacterSetCurrent(MovieStudioActor1);
+	if ((MovieStudioCurrentMovie == "Interview") && (MovieStudioCurrentScene == "1") && MouseIn(1250, 0, 500, 1000)) CharacterSetCurrent(MovieStudioActor2);
+}
+
+/**
+ * When the player needs to change clothes for a role in the movie
+ * @param {string} Cloth - The clothes to wear
+ * @returns {void} - Nothing
+ */
+function MovieStudioChange(Cloth) {
+	if (Cloth == "Journalist") {
+		CharacterNaked(Player);
+		InventoryWear(Player, "TeacherOutfit1", "Cloth", "Default");
+		InventoryWear(Player, "Glasses1", "Glasses", "#333333");
+		InventoryWear(Player, "Socks5", "Socks", "#444458");
+		InventoryWear(Player, "Shoes2", "Shoes", "#111111");
+	}
+}
+
+/**
+ * When the movie scene progresses, we assign the new values
+ * @param {string} Movie - The movie type
+ * @param {string} Scene - The scene in the movie
+ * @param {string} Role - Optional - The role the player is taking
+ * @returns {void} - Nothing
+ */
+function MovieStudioProgress(Movie, Scene, Role) {
+	MovieStudioTimer = CurrentTime;
+	MovieStudioCurrentMovie = Movie;
+	MovieStudioCurrentScene = Scene;
+	if (Role != "") MovieStudioCurrentRole = Role;
+	if ((Movie == "Interview") && (Scene == "1")) {
+		MovieStudioBackground = CommonRandomItemFromList("", ["BDSMRoomRed", "BDSMRoomBlue", "BDSMRoomPurple"]);
+		MovieStudioActor1 = CharacterLoadNPC("NPC_MovieStudio_Interview_Drawer");
+		MovieStudioActor1.FixedImage = "Screens/Room/MovieStudio/Drawer.png";
+		MovieStudioActor2 = CharacterLoadNPC("NPC_MovieStudio_Interview_XCross");
+		MovieStudioActor2.FixedImage = "Screens/Room/MovieStudio/XCross.png";
+	}
+	if (CurrentCharacter != null) DialogLeave();
 }
--- BondageClub/Scripts/Drawing.js
+++ BondageClub/Scripts/Drawing.js
@@ -178,6 +178,12 @@ function DrawArousalMeter(C, X, Y, Zoom) {
 function DrawCharacter(C, X, Y, Zoom, IsHeightResizeAllowed) {
 	if ((C != null) && ((C.ID == 0) || (Player.Effect.indexOf("BlindHeavy") < 0) || (CurrentScreen == "InformationSheet"))) {
 
+		// If there's a fixed image to draw instead of the character
+		if (C.FixedImage != null) {
+			DrawImageZoomCanvas(C.FixedImage, MainCanvas, 0, 0, 500, 1000, X, Y, 500 * Zoom, 1000 * Zoom);
+			return;
+		}
+
 		// Shortcuts drawing the character to 3D if needed
 		if (Draw3DEnabled) {
 			Draw3DCharacter(C, X, Y, Zoom, IsHeightResizeAllowed);
