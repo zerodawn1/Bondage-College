2510bfeacc258d947a4f4bc5c05dc9b0e8086a79
Ada18980 <72333272+Ada18980@users.noreply.github.com>
2020-11-20 07:49:56 -0800

    Futuristic Collar now locks itself (#1674)
    When you click "lock" it will lock the futuristic collar as well with the selected type of lock. unlock functionality is unchanged as you can always take the collar off manually, and most doms might prefer to keep the collar owner-locked permanently.

--- BondageClub/Screens/Inventory/ItemNeck/FuturisticCollar/FuturisticCollar.js
+++ BondageClub/Screens/Inventory/ItemNeck/FuturisticCollar/FuturisticCollar.js
@@ -159,7 +159,7 @@ function InventoryItemNeckFuturisticCollarValidate(C, Option) {
 
 function InventoryItemNeckFuturisticCollarLockdown(C, LockType) {
 	for (let E = C.Appearance.length - 1; E >= 0; E--)
-		if (((C.Appearance[E].Asset.Name.indexOf("Futuristic") >= 0 || C.Appearance[E].Asset.Name.indexOf("Interactive") >= 0) && C.Appearance[E].Asset.Group.Name != "ItemNeck") &&
+		if (((C.Appearance[E].Asset.Name.indexOf("Futuristic") >= 0 || C.Appearance[E].Asset.Name.indexOf("Interactive") >= 0) &&
 			(C.Appearance[E].Asset.AllowLock && InventoryGetLock(C.Appearance[E]) == null)) {
 				InventoryLock(C, C.Appearance[E], LockType, Player.MemberNumber);
 				var Lock = InventoryGetLock(C.Appearance[E])
