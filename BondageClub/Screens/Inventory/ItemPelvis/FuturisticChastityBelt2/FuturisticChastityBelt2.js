e86da56b44e5e54cff047989942e606d2eef9e50
Ada18980 <72333272+Ada18980@users.noreply.github.com>
2020-12-20 17:17:35 -0800

    Futuristic Chastity Belt Reskin (#1756)
    * Futuristic Chastity Belt Reskin
    
    Added Futuristic Chastity Panties, a new item that's based on the futuristic chastity belt. The only changes are visual, the coding is exactly the same.
    
    * Update
    
    * Increased contrast
    
    * Merge
    
    * Reset contrast

---
+++ BondageClub/Screens/Inventory/ItemPelvis/FuturisticChastityBelt2/FuturisticChastityBelt2.js
@@ -0,0 +1,35 @@
+"use strict";
+
+function InventoryItemPelvisFuturisticChastityBelt2Load() {
+	InventoryItemPelvisFuturisticChastityBeltLoad()
+}
+
+function InventoryItemPelvisFuturisticChastityBelt2Draw() {
+	InventoryItemPelvisFuturisticChastityBeltDraw()
+	
+}
+
+function InventoryItemPelvisFuturisticChastityBelt2Click() {
+	InventoryItemPelvisFuturisticChastityBeltClick()
+}
+
+function InventoryItemPelvisFuturisticChastityBelt2Exit() {
+	InventoryItemPelvisFuturisticChastityBeltExit()
+}
+
+
+function InventoryItemPelvisFuturisticChastityBelt2Validate(C) { 
+	return InventoryItemPelvisFuturisticChastityBeltValidate(C);
+}
+
+function InventoryItemPelvisFuturisticChastityBelt2NpcDialog(C, Option) { InventoryItemPelvisFuturisticChastityBeltNpcDialog(C, Option); }
+
+
+function AssetsItemPelvisFuturisticChastityBelt2ScriptUpdatePlayer(data) {
+	AssetsItemPelvisFuturisticChastityBeltScriptUpdatePlayer(data)
+}
+		
+// Update data
+function AssetsItemPelvisFuturisticChastityBelt2ScriptDraw(data) {
+	AssetsItemPelvisFuturisticChastityBeltScriptDraw(data) 
+}
