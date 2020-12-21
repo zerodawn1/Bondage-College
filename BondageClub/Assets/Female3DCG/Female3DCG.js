985068bf17ff3ab4f6650037a9dc6696aca6863b
Ruilov3 <63578707+Ruilov3@users.noreply.github.com>
2020-12-21 20:41:07 +0200

    Necklace (#1815)

--- BondageClub/Assets/Female3DCG/Female3DCG.js
+++ BondageClub/Assets/Female3DCG/Female3DCG.js
@@ -211,7 +211,13 @@ var AssetFemale3DCG = [
 				]
 
 			},
-		{ Name: "BlackHeart", Value: 40, Left: 148, Top: 70, ParentGroup: null }
+		{ Name: "BlackHeart", Value: 40, Left: 148, Top: 70, ParentGroup: null },
+		{ Name: "ElegantHeartNecklace", Value: 30, Left:155, Top: 152, ParentGroup: null,
+			Layer: [
+				{ Name: "Necklace", AllowColorize: true },
+				{ Name: "Jewels", AllowColorize: true },
+			]
+	},
 		],
 		Color: ["Default", "#202020", "#808080", "#bbbbbb", "#aa8080", "#80aa80", "#8080aa", "#aaaa80", "#80aaaa", "#aa80aa", "#cc3333", "#33cc33", "#3333cc", "#cccc33", "#33cccc", "#cc33cc"]
 	},
