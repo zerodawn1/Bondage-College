06cd3b0a265ecb797b3e70fe5af0d00a47d7ced8
Ellie <62729616+elliesec@users.noreply.github.com>
2021-01-11 15:10:22 +0000

    Fixes broken extended item menu for the clothing latex corset (#1872)

---
+++ BondageClub/Screens/Inventory/Corset/LatexCorset1/LatexCorset1.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// Loads the item extension properties
+function InventoryCorsetLatexCorset1Load() { InventoryItemTorsoLatexCorset1Load(true) }
+
+// Draw the item extension screen
+function InventoryCorsetLatexCorset1Draw() { InventoryItemTorsoLatexCorset1Draw(true) }
+
+// Catches the item extension clicks
+function InventoryCorsetLatexCorset1Click() { InventoryItemTorsoLatexCorset1Click(true) }
