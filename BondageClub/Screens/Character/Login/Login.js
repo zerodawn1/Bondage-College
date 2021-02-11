aa83e7d39b43c230c6c7d4a45a290df3c86d5eac
Ellie <62729616+elliesec@users.noreply.github.com>
2021-02-11 01:13:58 +0000

    Fix for disappearing lover/owner locks (#2066)

--- BondageClub/Screens/Character/Login/Login.js
+++ BondageClub/Screens/Character/Login/Login.js
@@ -451,7 +451,7 @@ function LoginResponse(C) {
 			LoginDifficulty();
 
 			// Loads the player character model and data
-			Player.Appearance = ServerAppearanceLoadFromBundle(Player, C.AssetFamily, C.Appearance);
+			Player.Appearance = ServerAppearanceLoadFromBundle(Player, C.AssetFamily, C.Appearance, C.MemberNumber);
 			InventoryLoad(Player, C.Inventory);
 			LogLoad(C.Log);
 			ReputationLoad(C.Reputation);
