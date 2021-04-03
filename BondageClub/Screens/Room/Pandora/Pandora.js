c6766f733781ec44ac3e766f33b1cf8763c664a7
Ben987 <30805159+Ben987@users.noreply.github.com>
2021-04-03 16:27:58 -0400

    Pandora's Box - Room Structure
    Pandora's Box - Room Structure

--- BondageClub/Screens/Room/Pandora/Pandora.js
+++ BondageClub/Screens/Room/Pandora/Pandora.js
@@ -1,6 +1,6 @@
 "use strict";
-var PandoraBackground = "PandoraMainHall";
-var PandoraCurrentRoom = 0;
+var PandoraBackground = "Pandora/Ground/Entrance";
+var PandoraCurrentRoom = null;
 var PandoraRoom = [];
 
 /**
@@ -14,13 +14,26 @@ function PandoraLoad() {
  * Runs and draws all Pandora's Box screens
  * @returns {void} - Nothing
  */
-function PandoraRun() {	
-	if (PandoraRoom.length == 0) return;
-	let Room = PandoraRoom[PandoraCurrentRoom];
-	let Pos = 750 - Room.Character.length * 250;
+function PandoraRun() {
+
+	// Gets the current room & background
+	if ((PandoraRoom.length == 0) || (PandoraCurrentRoom == null)) return;
+	PandoraBackground = "Pandora/" + PandoraCurrentRoom.Floor + "/" + PandoraCurrentRoom.Background;
+	
+	// Draws up to 4 characters in the room, including the player
+	let Pos = 690 - PandoraCurrentRoom.Character.length * 230;
 	DrawCharacter(Player, Pos, 0, 1);
-	for (let C = 0; C < Room.Character.length; C++)
-		DrawCharacter(Room.Character[C], Pos + ((C + 1) * 500), 0, 1);
+	let AllowMove = true;
+	for (let C = 0; C < PandoraCurrentRoom.Character.length; C++) {
+		DrawCharacter(PandoraCurrentRoom.Character[C], Pos + ((C + 1) * 460), 0, 1);
+		if ((PandoraCurrentRoom.Character[C].AllowMove != null) && (PandoraCurrentRoom.Character[C].AllowMove == false)) AllowMove = false;
+	}
+
+	// If we allow moving, we draw the paths
+	if (AllowMove)
+		for (let P = 0; P < PandoraCurrentRoom.Path.length; P++)
+			DrawButton(1885, 25 + P * 115, 90, 90, "", "White", "Icons/" + PandoraCurrentRoom.Direction[P] + ".png", TextGet("Direction" + PandoraCurrentRoom.Direction[P]));
+
 }
 
 /**
@@ -28,13 +41,50 @@ function PandoraRun() {
  * @returns {void} - Nothing
  */
 function PandoraClick() {
+
+	// Checks if we clicked on the player
 	if (PandoraRoom.length == 0) return;
-	let Room = PandoraRoom[PandoraCurrentRoom];
-	let Pos = 750 - Room.Character.length * 250;
+	let Pos = 690 - PandoraCurrentRoom.Character.length * 230;
 	if (MouseIn(Pos, 0, 500, 1000)) return CharacterSetCurrent(Player);
-	for (let C = 0; C < Room.Character.length; C++)
-		if (MouseIn(Pos + ((C + 1) * 500), 0, 500, 1000))
-			return CharacterSetCurrent(Room.Character[C]);
+
+	// Checks if we clicked on a character
+	let AllowMove = true;
+	for (let C = 0; C < PandoraCurrentRoom.Character.length; C++) {
+		if (MouseIn(Pos + ((C + 1) * 460), 0, 500, 1000)) return CharacterSetCurrent(PandoraCurrentRoom.Character[C]);
+		if ((PandoraCurrentRoom.Character[C].AllowMove != null) && (PandoraCurrentRoom.Character[C].AllowMove == false)) AllowMove = false;
+	}
+
+	// If we allow moving, we can switch room
+	if (AllowMove)
+		for (let P = 0; P < PandoraCurrentRoom.Path.length; P++)
+			if (MouseIn(1900, 25 + P * 115, 90, 90)) {
+				if (PandoraCurrentRoom.Path[P].Floor == "Exit") return CommonSetScreen("Room", "Infiltration");
+				return PandoraCurrentRoom = PandoraCurrentRoom.Path[P];
+			}
+
+}
+
+/**
+ * Loads the Pandora's Box screen
+ * @param {string} FloorName - The name of the floor in which we must generate rooms
+ * @param {object} Entry - The room object that's leading to that floor
+ * @returns {void} - Nothing
+ */
+function PandoraGenerateFloor(FloorName, EntryRoom) {
+	
+	// Always create the same entrance
+	let Room = {};
+	Room.Character = [];
+	Room.Floor = FloorName;
+	Room.Background = "Entrance";
+	Room.Path = [];
+	Room.Path.push(EntryRoom);
+	Room.Direction = [];
+	Room.Direction.push("StairsUp");
+	PandoraRoom.push(Room);
+	EntryRoom.Path.push(Room);
+	EntryRoom.Direction.push("StairsDown");
+	
 }
 
 /**
@@ -42,13 +92,42 @@ function PandoraClick() {
  * @returns {void} - Nothing
  */
 function PandoraBuildMainHall() {
-	PandoraCurrentRoom = 0;
+	
+	// Creates the ground entrance room
 	PandoraRoom = [];
 	let Room = {};
-	let Char = CharacterLoadNPC("NPC_Pandora_Maid");
+	let Char = CharacterLoadNPC("NPC_Pandora_EntranceMaid");
 	Char.AllowItem = false;
+	Char.AllowMove = false;
 	Room.Character = [];
 	Room.Character.push(Char);
-	Room.Background = "PandoraMainHall";
+	Room.Floor = "Ground";
+	Room.Background = "Entrance";
+	Room.Path = [];
 	PandoraRoom.push(Room);
+	
+	// Creates the exit room in the entrance
+	let ExitRoom = { Floor: "Exit" };
+	Room.Path.push(ExitRoom);
+	Room.Direction = [];
+	Room.Direction.push("Exit");
+	
+	// Generates the floors and sets the starting room
+	PandoraGenerateFloor("Underground", Room);
+	PandoraCurrentRoom = Room;
+	
+}
+
+/**
+ * Removes the current character from the room and deletes that NPC from the array
+ * @returns {void} - Nothing
+ */
+function PandoraRemoveCurrentCharacter() {
+	for (let C = 0; C < PandoraCurrentRoom.Character.length; C++)
+		if (PandoraCurrentRoom.Character[C].AccountName == CurrentCharacter.AccountName) {
+			PandoraCurrentRoom.Character.splice(C, 1);
+			break;
+		}
+	CharacterDelete(CurrentCharacter.AccountName);
+	DialogLeave();
 }
