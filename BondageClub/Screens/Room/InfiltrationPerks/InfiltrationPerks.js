c54ab22243d2d96508d92d1e759669c8fb8222f9
Ben987 <30805159+Ben987@users.noreply.github.com>
2021-04-14 20:56:52 -0400

    Pandora - Show only coded perks
    Pandora - Show only coded perks

--- BondageClub/Screens/Room/InfiltrationPerks/InfiltrationPerks.js
+++ BondageClub/Screens/Room/InfiltrationPerks/InfiltrationPerks.js
@@ -1,6 +1,7 @@
 "use strict";
 var InfiltrationPerksBackground = "Sheet";
-var InfiltrationPerksList = ["Strength", "Charisma", "Agility", "Resilience", "Endurance", "Lockpicking", "Investigation", "Running", "Mapping", "Forgery", "Begging", "Bribery", "Negotiation", "Recruiter"];
+//var InfiltrationPerksList = ["Strength", "Charisma", "Agility", "Resilience", "Endurance", "Lockpicking", "Investigation", "Running", "Mapping", "Forgery", "Begging", "Bribery", "Negotiation", "Recruiter"];
+var InfiltrationPerksList = ["Strength", "Charisma", "Agility", "Resilience", "Endurance", "Investigation", "Bribery", "Negotiation", "Recruiter"];
 
 /**
  * Checks if a named perk is activated or not
@@ -66,8 +67,6 @@ function InfiltrationPerksRun() {
 	MainCanvas.textAlign = "center";
 	DrawText(TextGet("Perks") + " " + InfiltrationPerksTaken().toString() + " / " + InfiltrationPerksAvail().toString(), 1430, 845, (InfiltrationPerksAvail() > InfiltrationPerksTaken()) ? "Blue" : "Black", "Gray");
 	DrawButton(1815, 75, 90, 90, "", "White", "Icons/Exit.png");
-	
-	DrawText("UNDER CONSTRUCTION", 1430, 750, "Red", "Gray");
 
 }
 
