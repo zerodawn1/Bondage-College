354740dfd4d726d75990745c3e98bf77d4253181
Ada18980 <72333272+Ada18980@users.noreply.github.com>
2021-04-22 11:21:29 -0700

    Kinky Dungeon Persistence (#2331)
    * Kinky Dungeon Persistent
    
    Makes the kinky dungeon minigame persistent so you can leave the game and come back to it without resetting your progress. To start a new game you have to either lose or relog.
    
    * Update BondageClub/Screens/MiniGame/KinkyDungeon/KinkyDungeon.js
    
    Co-authored-by: jomshir98 <jomshir98@protonmail.com>
    
    * Update BondageClub/Screens/MiniGame/KinkyDungeon/KinkyDungeon.js
    
    Co-authored-by: jomshir98 <jomshir98@protonmail.com>
    
    * Update BondageClub/Screens/MiniGame/KinkyDungeon/KinkyDungeon.js
    
    Co-authored-by: jomshir98 <jomshir98@protonmail.com>
    
    Co-authored-by: jomshir98 <jomshir98@protonmail.com>

--- BondageClub/Screens/MiniGame/KinkyDungeon/KinkyDungeon.js
+++ BondageClub/Screens/MiniGame/KinkyDungeon/KinkyDungeon.js
@@ -8,6 +8,8 @@ var KinkyDungeonKeybindings = null;
 var KinkyDungeonKeybindingsTemp = null;
 var KinkyDungeonKeybindingCurrentKey = 0;
 
+var KinkyDungeonGameRunning = false
+
 //var KinkyDungeonKeyLower = [87+32, 65+32, 83+32, 68+32, 81+32, 45+32, 90+32, 43+32]; // WASD
 var KinkyDungeonKey = [87, 65, 83, 68, 81, 45, 90, 43]; // WASD
 //var KinkyDungeonKeyNumpad = [56, 52, 50, 54, 55, 57, 49, 51]; // Numpad
@@ -33,32 +35,35 @@ function KinkyDungeonDressPlayer() {
  * @returns {void} - Nothing
  */
 function KinkyDungeonLoad() {
-	if (!KinkyDungeonPlayer)
-		KinkyDungeonPlayer = CharacterLoadNPC("NPC_Avatar");
 	
-	//KinkyDungeonCreateMap(MiniGameDifficulty);
-	var appearance = CharacterAppearanceStringify(Player)
-	CharacterAppearanceRestore(KinkyDungeonPlayer, appearance)
-	CharacterReleaseTotal(KinkyDungeonPlayer)
-	CharacterNaked(KinkyDungeonPlayer)
-	KinkyDungeonDressPlayer()
-	
-	KinkyDungeonKeybindings = Player.KinkyDungeonKeybindings
-	
-	KinkyDungeonState = "Menu"
-	
-	for (let G = 0; G < KinkyDungeonStruggleGroupsBase.length; G++) {
-		var group = KinkyDungeonStruggleGroupsBase[G]
-		if (group == "ItemM") {
-			if (InventoryGet(Player, "ItemMouth"))
-				KinkyDungeonRestraintsLocked.push("ItemMouth")
-			if (InventoryGet(Player, "ItemMouth2"))
-				KinkyDungeonRestraintsLocked.push("ItemMouth2")
-			if (InventoryGet(Player, "ItemMouth3"))
-				KinkyDungeonRestraintsLocked.push("ItemMouth3")
+	if (!KinkyDungeonGameRunning) {
+		if (!KinkyDungeonPlayer)
+			KinkyDungeonPlayer = CharacterLoadNPC("NPC_Avatar");
+		
+		//KinkyDungeonCreateMap(MiniGameDifficulty);
+		let appearance = CharacterAppearanceStringify(Player);
+		CharacterAppearanceRestore(KinkyDungeonPlayer, appearance)
+		CharacterReleaseTotal(KinkyDungeonPlayer)
+		CharacterNaked(KinkyDungeonPlayer)
+		KinkyDungeonDressPlayer()
+		
+		KinkyDungeonKeybindings = Player.KinkyDungeonKeybindings
+		
+		KinkyDungeonState = "Menu"
+		
+		for (let G = 0; G < KinkyDungeonStruggleGroupsBase.length; G++) {
+			let group = KinkyDungeonStruggleGroupsBase[G];
+			if (group == "ItemM") {
+				if (InventoryGet(Player, "ItemMouth"))
+					KinkyDungeonRestraintsLocked.push("ItemMouth")
+				if (InventoryGet(Player, "ItemMouth2"))
+					KinkyDungeonRestraintsLocked.push("ItemMouth2")
+				if (InventoryGet(Player, "ItemMouth3"))
+					KinkyDungeonRestraintsLocked.push("ItemMouth3")
+			}
+			if (InventoryGet(Player, group))
+				KinkyDungeonRestraintsLocked.push(group)
 		}
-		if (InventoryGet(Player, group))
-			KinkyDungeonRestraintsLocked.push(group)
 	}
 }
 
@@ -102,8 +107,10 @@ function KinkyDungeonRun() {
 		DrawButton(875, 750, 350, 64, TextGet("GameStart"), "White", "");
 		DrawButton(1275, 750, 350, 64, TextGet("GameConfigKeys"), "White", "");
 	} else if (KinkyDungeonState == "Game") {
+		KinkyDungeonGameRunning = true
 		KinkyDungeonDrawGame();
 	} else if (KinkyDungeonState == "End") {
+		KinkyDungeonGameRunning = false
 		// Draw temp start screen
 		DrawText(TextGet("EndWin"), 1250, 400, "white", "silver");
 		DrawText(TextGet("EndWin2"), 1250, 500, "white", "silver");
