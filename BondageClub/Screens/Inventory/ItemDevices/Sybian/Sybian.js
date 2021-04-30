3746a4ab709770613eee2df49696c0b2e5b981d4
SandrineBC <61221552+SandrineBC@users.noreply.github.com>
2021-04-30 22:34:24 +0200

    Exetended Vibrator Script for Sybian (#2379)

--- BondageClub/Screens/Inventory/ItemDevices/Sybian/Sybian.js
+++ BondageClub/Screens/Inventory/ItemDevices/Sybian/Sybian.js
@@ -1,44 +1,17 @@
 "use strict";
 
-// Loads the item extension properties
 function InventoryItemDevicesSybianLoad() {
-	if (DialogFocusItem.Property == null) DialogFocusItem.Property = { Intensity: -1 };
+	VibratorModeLoad([VibratorModeSet.STANDARD, VibratorModeSet.ADVANCED]);
 }
 
-// Draw the item extension screen
 function InventoryItemDevicesSybianDraw() {
-	const Intensity = DialogFocusItem.Property.Intensity;
-	DrawAssetPreview(1387, 225, DialogFocusItem.Asset, {Vibrating: Intensity >= 0});
-
-	DrawText(DialogFindPlayer("Intensity" + Intensity.toString()).replace("Item", DialogFocusItem.Asset.Description), 1500, 600, "White", "Gray");
-	if (Intensity !== -1) DrawButton(1200, 650, 200, 55, DialogFindPlayer("TurnOff"), "White");
-	if (Intensity !== 0) DrawButton(1550, 650, 200, 55, DialogFindPlayer("Low"), "White");
-	if (Intensity !== 1) DrawButton(1200, 710, 200, 55, DialogFindPlayer("Medium"), "White");
-	if (Intensity !== 2) DrawButton(1550, 710, 200, 55, DialogFindPlayer("High"), "White");
-	if (Intensity !== 3) DrawButton(1375, 770, 200, 55, DialogFindPlayer("Maximum"), "White");
+	VibratorModeDraw([VibratorModeSet.STANDARD, VibratorModeSet.ADVANCED]);
 }
 
-// Catches the item extension clicks
 function InventoryItemDevicesSybianClick() {
-	if (MouseIn(1885, 25, 90, 90)) DialogFocusItem = null;
-	else if (MouseIn(1200, 650, 200, 55) && (DialogFocusItem.Property.Intensity > -1)) InventoryItemDevicesSybianSetIntensity(-1 - DialogFocusItem.Property.Intensity);
-	else if (MouseIn(1550, 650, 200, 55) && (DialogFocusItem.Property.Intensity < 0)) InventoryItemDevicesSybianSetIntensity(0 - DialogFocusItem.Property.Intensity);
-	else if (MouseIn(1550, 650, 200, 55) && (DialogFocusItem.Property.Intensity > 0)) InventoryItemDevicesSybianSetIntensity(0 - DialogFocusItem.Property.Intensity);
-	else if (MouseIn(1200, 710, 200, 55) && (DialogFocusItem.Property.Intensity < 1)) InventoryItemDevicesSybianSetIntensity(1 - DialogFocusItem.Property.Intensity);
-	else if (MouseIn(1200, 710, 200, 55) && (DialogFocusItem.Property.Intensity > 1)) InventoryItemDevicesSybianSetIntensity(1 - DialogFocusItem.Property.Intensity);
-	else if (MouseIn(1550, 710, 200, 55) && (DialogFocusItem.Property.Intensity > 2)) InventoryItemDevicesSybianSetIntensity(2 - DialogFocusItem.Property.Intensity);
-	else if (MouseIn(1550, 710, 200, 55) && (DialogFocusItem.Property.Intensity < 2)) InventoryItemDevicesSybianSetIntensity(2 - DialogFocusItem.Property.Intensity);
-	else if (MouseIn(1375, 770, 200, 55) && (DialogFocusItem.Property.Intensity < 3)) InventoryItemDevicesSybianSetIntensity(3 - DialogFocusItem.Property.Intensity);
+	VibratorModeClick([VibratorModeSet.STANDARD, VibratorModeSet.ADVANCED]);
 }
 
-// Sets the vibrating intensity
-function InventoryItemDevicesSybianSetIntensity(Modifier) {
-	var C = (Player.FocusGroup != null) ? Player : CurrentCharacter;
-	DialogFocusItem.Property.Intensity = DialogFocusItem.Property.Intensity + Modifier;
-	if (DialogFocusItem.Property.Intensity == -1) DialogFocusItem.Property.Effect = ["Egged"];
-	if (DialogFocusItem.Property.Intensity >= 0) DialogFocusItem.Property.Effect = ["Egged", "Vibrating"];
-	CharacterLoadEffect(C);
-	if (C.ID == 0) ServerPlayerAppearanceSync();
-
-	ChatRoomPublishCustomAction("Sybian" + ((Modifier > 0) ? "Increase" : "Decrease") + "To" + DialogFocusItem.Property.Intensity, true, [{Tag: "DestinationCharacterName", Text: C.Name, MemberNumber: C.MemberNumber}]);
+function AssetsItemDevicesSybianScriptDraw(data) {
+	VibratorModeScriptDraw(data);
 }
